1868a176688a3e839be235c786aa4939e29538b4
Santos A. Perez <89759017+Santos-A-PerezUTexas@users.noreply.github.com>
2021-09-01 21:28:54 -0400

    Update train.py

--- homework1/homework/train.py
+++ homework1/homework/train.py
@@ -1,7 +1,45 @@
 from .models import ClassificationLoss, model_factory, save_model, LinearClassifier, MLPClassifier
 from .utils import accuracy, load_data
 import torch
+"""
+********models.py has these classes:
 
+ClassificationLoss(torch.nn.Module)
+  forward(self, input, target)
+
+LinearClassifier(torch.nn.Module), 
+  __init__, 
+   forward(self, x)
+
+MLPClassifier(torch.nn.Module), 
+  __init__, 
+  forward(self, x)
+
+models.py defines these functions:
+
+save_model(model)
+load_model(model)
+
+***************************utils.py has these classes/functions
+
+class SuperTuxDataset(Dataset)
+    def __init__(self, dataset_path)
+    def __len__(self)
+    def __getitem__(self, idx)
+    
+def load_data(dataset_path, num_workers=0, batch_size=128)
+def accuracy(outputs, labels)
+
+ANd utils.py defines this (dictionary), which is imported above:
+
+model_factory = {
+    'linear': LinearClassifier,
+    'mlp': MLPClassifier,
+}
+
+
+
+"""
 def train(args):
     model = model_factory[args.model]()
 
