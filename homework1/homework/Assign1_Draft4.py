21396d45808447fdbc8bbbc0a6c3167cc0762aea
sperezlaw <89759017+Santos-A-PerezUTexas@users.noreply.github.com>
2021-09-17 14:50:12 -0400

    Update

--- homework1/homework/Assign1_Draft4.py
+++ homework1/homework/Assign1_Draft4.py
@@ -1,6 +1,6 @@
 import torch
 import torch.nn.functional as F
-from torch.nn.parameter import Parameter
+from torch.nn.parameter import Parameter          #USE THIS!!!!!!!!!!!!!!!!!!   SEPT 17, 2021
 from PIL import Image
 from torch.utils.data import Dataset, DataLoader
 from torchvision import transforms as Image_Transformer
@@ -129,11 +129,19 @@ class ClassificationLoss(torch.nn.Module):
     Negative_L_loss = torch.nn.NLLLoss()
     cross_loss = torch.nn.CrossEntropyLoss()
  
-    #nn.CrossEntropyLoss() is actually a combination of nn.LogSoftmax() and nn.NLLLoss(). Refer this 5 doc.
-    #weighted_mean_batch_loss = Negative_L_loss(Get_Softmax(Y_hat_Vector),  y_vector)
-    #weighted_mean_batch_loss = Negative_L_loss(Y_hat_Vector,  y_vector)
  
+    print (f'SEPT 17, 2021:    INSIDE OF THE LOSS FUNCTION OBJECT')
+    print (f'SEPT 17, 2021:    Y_hat_vector predictions is {Y_hat_Vector}, and y_vector (labels) is {y_vector} ')
+    print (f'SEPT 17, 2021:    Y_hat_vector should have grad_fn=<CopySlices> , and y_vector no gradient!')
+    
+    
     weighted_mean_batch_loss = cross_loss(Y_hat_Vector,  y_vector)
+    
+    
+    ##y_hat_Vector has grad_fn=<CopySlices>
+    ##y_vector has NO GRAD!!!!!!!!!!!!!  PROBLEM????????
+      
+    
     
     
     #Y_hat_vector is a minibatch with 128 6-tensor entries for 128 (batch_size) image outputs
@@ -141,9 +149,11 @@ class ClassificationLoss(torch.nn.Module):
     #Get_log_Softmax returns 128 6-tensor entries with 6 log probabilities for each image
     #Negative_L_Loss returns a 1 tensor entry with the mean? loss????, with backward you get gradients, then optimize.step
         
-    print (f'*********INSIDE Classification Loss, Y_hat_Vector vector is {Y_hat_Vector}, and this compares with the actual values{y_vector}')
+    #print (f'*********INSIDE Classification Loss, Y_hat_Vector vector is {Y_hat_Vector}, and this compares with the actual values{y_vector}')
     
     print (f'*********STILL INSIDE Classification Loss, the weighted_mean_batch_loss is {weighted_mean_batch_loss}')
+    #NO GRADIENT ---- PROBLEM!!!!!!!!!!!
+    
     
     #print (f'*********The backward fx of weighted_mean_batch_loss (gradient) is {weighted_mean_batch_loss.backward(retain_graph=True)}')
         
@@ -165,12 +175,18 @@ class LinearClassifier(torch.nn.Module):
        
     self.input_dim = input_dim
     
+    #added this Sept 17, 2021
+    self.w = Parameter(torch.ones(input_dim))
+    self.b = Parameter(-torch.ones(1))
+    
+    
     self.network = torch.nn.Linear(input_dim, 6)
     
      
   def forward(self, image_tensor):   
     
     flatened_Image_tensor = image_tensor.view(image_tensor.size(0), -1).view(-1)
+    #this will have NO GRADIENT!
     
     return self.network(flatened_Image_tensor)
   
@@ -189,6 +205,10 @@ class MLPClassifier(torch.nn.Module):
    
     super().__init__()        
     self.hidden_size = hidden_size
+    
+    #added this Sept 17, 2021
+    self.w = Parameter(torch.ones(input_dim))
+    self.b = Parameter(-torch.ones(1))
         
     self.network = torch.nn.Sequential( 
                 torch.nn.Linear(input_dim, hidden_size),   #----->keep this layer small to save parameters???
@@ -198,10 +218,23 @@ class MLPClassifier(torch.nn.Module):
                 
   def forward(self, image_tensor):   
   
-    flatened_Image_tensor = image_tensor.view(image_tensor.size(0), -1).view(-1)
     
-    return self.network(flatened_Image_tensor)
-  
+    flattened_Image_tensor = image_tensor.view(image_tensor.size(0), -1).view(-1)
+    
+    print(f'SEPT 17:  Inside forward of MLPx, line 223, flattened_image x-value is {flattened_Image_tensor}, image_tensor orginal is {image_tensor}')
+    #SEPT 17, 2021: image_tensor HAS NO GRADIENT!
+    #SEPT 17, 2021: image_tensor HAS NO GRADIENT!
+    #SEPT 17, 2021: image_tensor HAS NO GRADIENT!
+    
+    
+    print(f'the VIEW of the image tensor is {image_tensor.view(image_tensor.size(0), -1).view(-1)}')
+    
+    print(f'Now will compute y_hat with the flattened image, this is y_hat: {self.network(flattened_Image_tensor)}')
+    print(f"I will now return that value to the training loop")
+    
+    return self.network(flattened_Image_tensor)
+    # This result grad_fn=<AddBackward0>, but flattened_image (x) has no grad!
+    
   #def forward(self, multiple_image_tensor):   
   #receives a (B,3,64,64) tensor as an input and should return a (B,6) torch.Tensor
        
@@ -290,43 +323,83 @@ def train(args):
     
     #flatened_Image_tensor = torch.rand(input_dim)
     
+#------------------------------BEGIN ITERATE THROUGH BATCHES------------------------------------------
+
     for batch_idx, image_tuples_tensor in enumerate(Tux_DataLoader):  #iterate batches
            
+      print (f'image_tuples_tensor[1], the labels for this batch[{batch_idx}], is {image_tuples_tensor[1]} ')
+      val = input(f'At beggining of for loop [{batch_idx}] for batches, iterating through all batches')
+      print(val)
+      
+      #------------------------------BEGIN ITERATE THROUGH ALL IMAGES OF A BATCH
+      
       for i in range(len(image_tuples_tensor[0])):  #iterate through all images, MAYBE I DONT NEED THIS!
         #print (f'label {i}, for batch {batch_idx} is {image_tuples_tensor[1][i]}')
         
         y_hat_tensorLinear[i] = linear_M(image_tuples_tensor[0][i]) 
         
-        y_hat_tensorMLP[i] = MLPx(image_tuples_tensor[0][i])
-        
-        #y_hat_tensorMLP[i] = MLPx(flatened_Image_tensor)
+        y_hat_tensorMLP[i] = MLPx(image_tuples_tensor[0][i])   #flattening occurs in MLPx
         #y_hat_tensorMLP[i] = MLPx(flatened_Image_tensor)   "a view requires grad in place operation"
-        #y_hat_tensorMLP[i] = MLPx(flatened_Image_tensor)
+        #This may be an "inplace" operation!!!!!!!!
         
-        print (f'%%%%%%%%  Just assinged y_hat_tensor[{i}] this value:   {MLPx(image_tuples_tensor[0][i])}')
-      
-      #end iterate through all images
+        print(f'At line 302, y_hat_tensorMLP[i] {y_hat_tensorMLP[i]} has been assigned the MLPx of image_tuples_tensor[0][i], {image_tuples_tensor[0][i]}')
+        
+        #y_hat_tensorMLP[i] has grad_fn=<SelectBackward>  
+        #BUT ..... image_tuples_tensor[0][i] - the "input" x_vector has no grad!!!!!!!!!!
+        #MLPx flatens this X image tensor then runs it through the network to get Y_hat - need grad!
+        
+       
+        
+        print (f'%%%%%%%%  Just assigned y_hat_tensor[{i}] this value:   {MLPx(image_tuples_tensor[0][i])}')
+        #Same tensor as line 302, but with grad_fn=<AddBackward0> (for MLPx(image_tuples_tensor[0][i]))
+        
+        
+    #-------------------------END ITERATE THROUGH ALL IMAGES OF A BATCH --------------------------
       
       #print ('batch idx{}, batch len {}'.format(batch_idx, len(image_tuples_tensor)))
       #print (f'------------This is batch {batch_idx}, it has size {len(image_tuples_tensor)}, and here is the image_tuples_tensor: {image_tuples_tensor}')
       #print (f'------------The size of image_tuples_tensor[0] for batch[{batch_idx}], the images, is {len(image_tuples_tensor[0])}')
       #print (f'------------The size of image_tuples_tensor[1] for batch[{batch_idx}], the labels, is {len(image_tuples_tensor[1])}')
       #print (f'These are the labels for this batch:  {image_tuples_tensor[1]}')
-           
+      
+      print (f'SEPT 17:  444444444444444444444---------------------->CURRENTLY AT BACTH[{batch_idx}]') 
+      print (f'SEPT 17:  444444444444444444444---------------------->CURRENTLY AT BACTH[{batch_idx}]') 
+      print (f'SEPT 17:  444444444444444444444---------------------->CURRENTLY AT BACTH[{batch_idx}]') 
+      print (f'SEPT 17:  444444444444444444444---------------------->CURRENTLY AT BACTH[{batch_idx}]') 
+          
       val = input(f'!!!!!!!!!!!!!!!!About to call model_loss with this value for y_hat {y_hat_tensorMLP}, and this one for target,   {image_tuples_tensor[1]}')
       print(val)
       
+      #y_hat has grad_fn=<CopySlices>)
+      #target, images_tuples_tensor[1] has no grad!!!  (the labels)
       
-      model_loss = calculate_loss(y_hat_tensorMLP, image_tuples_tensor[1]) 
+      model_lossLinear = calculate_loss(y_hat_tensorLinear, image_tuples_tensor[1])   
+      model_lossMLP = calculate_loss(y_hat_tensorMLP, image_tuples_tensor[1])   #8x8x8x8x8x8x8x8x8x8x8x8x8x8x8x8  HERE
+      ##y_hat_tensorMLP has grad_fn=<CopySlices>
+      ##image_tuples_tensor[1] has NO GRAD!!!!!!!!!!!!!  PROBLEM????????
       
       torch.autograd.set_detect_anomaly(True)
       
+      
       optimizerMLPx.zero_grad()
-      model_loss.backward(retain_graph=True) 
+      optimizerLinear.zero_grad()
+      
+      model_lossLinear.backward(retain_graph=True)
+      #model_lossMLP.backward(retain_graph=True)    # #8x8x8x8x8x8x8x8x8x8x8x8x8x8x8x8  PROBLEM HERE
+      
+      #one of the variables needed for gradient computation has been modified by an inplace operation: 
+      #[torch.FloatTensor [5, 6]], which is output 0 of TBackward, is at version 2; expected version 1 instead. 
+      #Hint: the backtrace further above shows the operation that failed to compute its gradient. 
+      #The variable in question was changed in there or anywhere later
+      
+      
+      optimizerLinear.step()
       optimizerMLPx.step()
         
-      print (f'==============================LOSS FOR BATCH {batch_idx} is {model_loss} and the gradient is deleted')
-            
+      print (f'==============================MLP LOSS FOR BATCH {batch_idx} is {model_lossMLP} and the gradient is deleted')
+           
+      print (f'==============================Linear LOSS FOR BATCH {batch_idx} is {model_lossLinear} and the gradient is deleted')
+      
       print ("#### NOW calculate the loss, obtain gradients, and update weights BEFORE next batch")
       
       val = input(f'##################  Above you can see batch {batch_idx}  #################')
@@ -365,6 +438,8 @@ def train(args):
     
         #p.data[:] -= 0.5 * p.grad                    
         #p.grad.zero_()
+#------------------------------END ITERATE THROUGH BATCHES------------------------------------------
+
 
 #*********************************************************END TRAINING*************************************************************
 #*********************************************************END TRAINING*************************************************************
