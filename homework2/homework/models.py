ff5f51bc66f972d72ae7b6ff5f0861fcb7f829a8
sperezlaw <89759017+Santos-A-PerezUTexas@users.noreply.github.com>
2021-09-21 14:09:12 -0400

    Update

--- homework2/homework/models.py
+++ homework2/homework/models.py
@@ -3,82 +3,39 @@ import torch
 
 class CNNClassifier(torch.nn.Module):
     def __init__(self):
-        """
-        CNNClassifier. Similar to homework 1, model should return a (B,6) Tensor which represents the logits of the classes. Use convolutions this time.
+      super().__init__()
 
-https://pytorch.org/docs/stable/nn.html#torch.nn.Conv2d
-https://stackoverflow.com/questions/42883547/intuitive-understanding-of-1d-2d-and-3d-convolutions-in-convolutional-neural-n
-
-https://www.reddit.com/r/deeplearning/comments/hfjiog/what_is_1d2d_and_3d_convolutions_in_cnn/
-
-    A 1D convolution is for time series and it is a WxC filter that slides in the W direction. W represents 
-    the time dimension and C represents multivariate dimensions.
-
-    A 2D convolution is for images and it is a HxWxC filter that slides in the H and W directions. 
-    C indicates the Channel (RGB) for the first layer, and number of input feature maps for higher layers.
-
-    A 3D convolution is for voxel data and it is a DxHxWxC filter th torch.nn.Conv2d
-            
-https://medium.com/thecyphy/train-cnn-model-with-pytorch-21dafb918f48
-https://medium.com/thecyphy/train-cnn-model-with-pytorch-21dafb918f48
-
-        Your code here
-def __init__(self):
-        super().__init__()
-
-        self.network = nn.Sequential(
-            
-            nn.Conv2d(3, 32, kernel_size = 3, padding = 1),
-            nn.ReLU(),
-            nn.Conv2d(32,64, kernel_size = 3, stride = 1, padding = 1),
-            nn.ReLU(),
-            nn.MaxPool2d(2,2),
-        
-            nn.Conv2d(64, 128, kernel_size = 3, stride = 1, padding = 1),
-            nn.ReLU(),
-            nn.Conv2d(128 ,128, kernel_size = 3, stride = 1, padding = 1),
-            nn.ReLU(),
-            nn.MaxPool2d(2,2),
-            
-            nn.Conv2d(128, 256, kernel_size = 3, stride = 1, padding = 1),
-            nn.ReLU(),
-            nn.Conv2d(256,256, kernel_size = 3, stride = 1, padding = 1),
-            nn.ReLU(),
-            nn.MaxPool2d(2,2),
-            
-            nn.Flatten(),
-            nn.Linear(82944,1024),
-            nn.ReLU(),
-            nn.Linear(1024, 512),
-            nn.ReLU(),
-            nn.Linear(512,6)
-        )
+      self.network = torch.nn.Sequential(
+                  
+                  torch.nn.Conv2d(3, 32, kernel_size = 3, padding = 1),
+                  torch.nn.ReLU(),
+                  torch.nn.Conv2d(32,64, kernel_size = 3, stride = 1, padding = 1),
+                  torch.nn.ReLU(),
+                  torch.nn.MaxPool2d(2,2),
+              
+                  torch.nn.Conv2d(64, 128, kernel_size = 3, stride = 1, padding = 1),
+                  torch.nn.ReLU(),
+                  torch.nn.Conv2d(128 ,128, kernel_size = 3, stride = 1, padding = 1),
+                  torch.nn.ReLU(),
+                  torch.nn.MaxPool2d(2,2),
+                  
+                  torch.nn.Conv2d(128, 256, kernel_size = 3, stride = 1, padding = 1),
+                  torch.nn.ReLU(),
+                  torch.nn.Conv2d(256,256, kernel_size = 3, stride = 1, padding = 1),
+                  torch.nn.ReLU(),
+                  torch.nn.MaxPool2d(2,2),
+                  
+                  torch.nn.Flatten(),
+                  torch.nn.Linear(82944,1024),
+                  torch.nn.ReLU(),
+                  torch.nn.Linear(1024, 512),
+                  torch.nn.ReLU(),
+                  torch.nn.Linear(512,6)
+              )
     
-                    COMMENTS
-                    ---------
-    I think we can use an example here, assume we have a small image [3(channels),8(height),8(width)]?[3,8,8]
-We want to do 5 classifications for each pixel in this image. So it will be: lnput[3󭅌]?Output[8󭅉],
-
-If we use linear classification, it will be a linear layer: linear[input:3,output:5].
-
-If we use 1�1conv, it will be  conv2d(in_channel:3,out_channel:5
-
-http://www.philkr.net/dl_class/lectures/convolutional_networks/03.html
-https://www.youtube.com/watch?v=p1xZ2yWU1eo
-
-
-        """
-
     def forward(self, x):
-        """
-        Your code here
-        @x: torch.Tensor((B,3,64,64))
-        @return: torch.Tensor((B,6))
-        
-        def forward(self, xb):
-        return self.network(xb)        
-
-        """
+     return self.network(x)
+  
         
 
 def save_model(model):
