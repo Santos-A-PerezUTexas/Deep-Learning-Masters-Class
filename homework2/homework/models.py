5b77f51579b8a985af8064bb603c7a2e445606e7
sperezlaw <89759017+Santos-A-PerezUTexas@users.noreply.github.com>
2021-09-20 21:58:02 -0400

    Update

--- homework2/homework/models.py
+++ homework2/homework/models.py
@@ -4,6 +4,20 @@ import torch
 class CNNClassifier(torch.nn.Module):
     def __init__(self):
         """
+        CNNClassifier. Similar to homework 1, model should return a (B,6) Tensor which represents the logits of the classes. Use convolutions this time.
+
+https://pytorch.org/docs/stable/nn.html#torch.nn.Conv2d
+https://stackoverflow.com/questions/42883547/intuitive-understanding-of-1d-2d-and-3d-convolutions-in-convolutional-neural-n
+
+https://www.reddit.com/r/deeplearning/comments/hfjiog/what_is_1d2d_and_3d_convolutions_in_cnn/
+
+A 1D convolution is for time series and it is a WxC filter that slides in the W direction. W represents the time dimension and C represents multivariate dimensions.
+
+A 2D convolution is for images and it is a HxWxC filter that slides in the H and W directions. C indicates the Channel (RGB) for the first layer, and number of input feature maps for higher layers.
+
+A 3D convolution is for voxel data and it is a DxHxWxC filter th
+torch.nn.Conv2d
+        
         Your code here
         """
         raise NotImplementedError('CNNClassifier.__init__')
