cd0b2df0db907c6f3fe139aa8d87b8a513681838
sperezlaw <89759017+Santos-A-PerezUTexas@users.noreply.github.com>
2021-09-22 21:38:49 -0400

    Update

--- homework2/homework/models.py
+++ homework2/homework/models.py
@@ -1,9 +1,18 @@
 import torch
 
 #https://adventuresinmachinelearning.com/convolutional-neural-networks-tutorial-in-pytorch/
+#calculate dimensions https://docs.google.com/spreadsheets/d/1UUromHy5ksKlcFafpJ760HoZ-vEwAJcoCKG0U80K5L8/edit#gid=0
 
+"""
+Formula [(W-K+2P)/S]+1.
+
+W is the input volume - in your case 128  (image size, my case 64)
+K is the Kernel size - in your case 5
+P is the padding - in your case 0 i believe
+S is the stride - which you have not provided.
 class CNNClassifier(torch.nn.Module):
 
+"""
     def __init__(self, layers=[], n_input_channels=3, kernel_size=3):
       
       super().__init__()
@@ -13,22 +22,32 @@ class CNNClassifier(torch.nn.Module):
        
       self.layer1 = nn.Sequential(
       
-            nn.Conv2d(c, 32, kernel_size=5, stride=1, padding=2),    #32 output channels, c input channels, output is 14x14 for 28x28 image!
+            nn.Conv2d(c, 32, kernel_size=5, stride=1, padding=2),    #32 output channels, c input channels, output is 64x64
             nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2, stride=2))
+            nn.MaxPool2d(kernel_size=2, stride=2))    #This produces 32x32!!! 
             
       self.layer2 = nn.Sequential(
-            nn.Conv2d(32, 64, kernel_size=5, stride=1, padding=2),  #32 input channels from layer1, 64 output channels
+            nn.Conv2d(32, 64, kernel_size=5, stride=1, padding=2),  #32 input channels from layer1 (32x32 dims also), 64 output channels
             nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2, stride=2))                  #down-sampling, or pooling, to produce a 7 x 7 output of layer 2.
+            nn.MaxPool2d(kernel_size=2, stride=2))                  #down-sampling, or pooling, to produce a 16 x 16 output of layer 2 (reducing from 32x32).
         
       #self.drop_out = nn.Dropout()
         
-      self.fc1 = nn.Linear(7 * 7 * 64, 1000)   #this takes 7x7 of layer 2
+      self.fc1 = nn.Linear(32 * 32 * 64, 100)   #this takes 7x7 of layer 2
         
-      self.fc2 = nn.Linear(1000, 6)      #10 OUTPUTS, Changed to 6
+      self.fc2 = nn.Linear(100, 6)      #10 OUTPUTS, Changed to 6
     
-    def forward(self, x):
+    
+    def forward(self, images_batch):
+    
+    batch_size=128
+    input_dim = 3   
+    x = torch.zeros(batch_size, 6)
+    
+    for i in range(batch_size):
+    
+       x[i] = self.network(images_batch[i].view(image_tensor[i].size(0), -1).view(-1))
+      #x[i] = self.network(images_batch[i].reshape(1, input_dim))
     
       out = self.layer1(x)
       out = self.layer2(out)
