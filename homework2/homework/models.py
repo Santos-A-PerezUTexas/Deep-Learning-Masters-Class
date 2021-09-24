256fe78e74227837e4c26b9112e1374b92a9635e
Santos-A-PerezUTexas <saperez@utexas.edu>
2021-09-24 00:44:29 +0000

    Update From Colab

--- homework2/homework/models.py
+++ homework2/homework/models.py
@@ -23,9 +23,9 @@ class CNNClassifier(torch.nn.Module):
        
       self.layer1 = torch.nn.Sequential(
       
-            torch.nn.Conv2d(c, 32, kernel_size=5, stride=1, padding=2),#32 output channels, 3 input channels, output is 64x64
+            torch.nn.Conv2d(c, 32, kernel_size=5, stride=1, padding=2),#output is 32 channels of 64x64 images
             torch.nn.ReLU(),
-            torch.nn.MaxPool2d(kernel_size=2, stride=2))    #This produces 32x32 output 
+            torch.nn.MaxPool2d(kernel_size=2, stride=2))    #This produces 32x32 image and 32 channels
             
       self.layer2 = torch.nn.Sequential(
             torch.nn.Conv2d(32, 64, kernel_size=5, stride=1, padding=2),  #32 input channels from layer1 (32x32 dims also), 64 output channels
@@ -34,20 +34,20 @@ class CNNClassifier(torch.nn.Module):
         
       #self.drop_out = nn.Dropout()
         
-      self.fc1 = torch.nn.Linear(32 * 32 * 64, 100)   #this takes 32x32 of layer 2
+      self.fc1 = torch.nn.Linear(32 * 32 * 32, 6)   #this takes 32x32 image of layer1 or 2, 32 channels
         
-      self.fc2 = torch.nn.Linear(100, 6)      #10 OUTPUTS, Changed to 6
+      #self.fc2 = torch.nn.Linear(100, 6)      #10 OUTPUTS, Changed to 6
     
     
     def forward(self, images_batch):
     
      
       out = self.layer1(images_batch)
-      out = self.layer2(out)
+      #out = self.layer2(out)
       out = out.reshape(out.size(0), -1)
       #out = self.drop_out(out)
       out = self.fc1(out)
-      out = self.fc2(out)
+      #out = self.fc2(out)
                    
       return out
     #Expected 4-dimensional input for 4-dimensional weight [32, 3, 5, 5], but got 3-dimensional input of size [3, 64, 64] instead 
