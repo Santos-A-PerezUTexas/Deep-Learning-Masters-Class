d4a314faee48f676c623ae3dd305ae8db6434453
sperezlaw <89759017+Santos-A-PerezUTexas@users.noreply.github.com>
2021-09-30 15:01:02 -0400

    Update

---
+++ homework3/homework/models.py
@@ -0,0 +1,71 @@
+import torch
+import torch.nn.functional as F
+
+
+class CNNClassifier(torch.nn.Module):
+    def __init__(self):
+        super().__init__()
+        """
+        Your code here
+        Hint: Base this on yours or HW2 master solution if you'd like.
+        Hint: Overall model can be similar to HW2, but you likely need some architecture changes (e.g. ResNets)
+        """
+        raise NotImplementedError('CNNClassifier.__init__')
+
+    def forward(self, x):
+        """
+        Your code here
+        @x: torch.Tensor((B,3,64,64))
+        @return: torch.Tensor((B,6))
+        Hint: Apply input normalization inside the network, to make sure it is applied in the grader
+        """
+        raise NotImplementedError('CNNClassifier.forward')
+
+
+class FCN(torch.nn.Module):
+    def __init__(self):
+        super().__init__()
+        """
+        Your code here.
+        Hint: The FCN can be a bit smaller the the CNNClassifier since you need to run it at a higher resolution
+        Hint: Use up-convolutions
+        Hint: Use skip connections
+        Hint: Use residual connections
+        Hint: Always pad by kernel_size / 2, use an odd kernel_size
+        """
+        raise NotImplementedError('FCN.__init__')
+
+    def forward(self, x):
+        """
+        Your code here
+        @x: torch.Tensor((B,3,H,W))
+        @return: torch.Tensor((B,6,H,W))
+        Hint: Apply input normalization inside the network, to make sure it is applied in the grader
+        Hint: Input and output resolutions need to match, use output_padding in up-convolutions, crop the output
+              if required (use z = z[:, :, :H, :W], where H and W are the height and width of a corresponding strided
+              convolution
+        """
+        raise NotImplementedError('FCN.forward')
+
+
+model_factory = {
+    'cnn': CNNClassifier,
+    'fcn': FCN,
+}
+
+
+def save_model(model):
+    from torch import save
+    from os import path
+    for n, m in model_factory.items():
+        if isinstance(model, m):
+            return save(model.state_dict(), path.join(path.dirname(path.abspath(__file__)), '%s.th' % n))
+    raise ValueError("model type '%s' not supported!" % str(type(model)))
+
+
+def load_model(model):
+    from torch import load
+    from os import path
+    r = model_factory[model]()
+    r.load_state_dict(load(path.join(path.dirname(path.abspath(__file__)), '%s.th' % model), map_location='cpu'))
+    return r
