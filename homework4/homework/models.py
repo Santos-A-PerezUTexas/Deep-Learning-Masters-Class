f09372d671a4b3eeeb1c32480b205953193a2760
Santos-A-PerezUTexas <saperez@utexas.edu>
2021-10-29 22:10:22 +0000

    Update From Colab

--- homework4/homework/models.py
+++ homework4/homework/models.py
@@ -25,40 +25,119 @@ def extract_peak(heatmap, max_pool_ks=7, min_score=-5, max_det=100):
        @return: List of peaks [(score, cx, cy), ...], where cx, cy are the position of a peak and score is the
                 heatmap value at the peak. Return no more than max_det peaks per image
     """
-    raise NotImplementedError('extract_peak')
+    return("You have succesfully called extract_peak as follows:  Detector-->Forward()--->Detect()--->extrac_peak")
 
 
 class Detector(torch.nn.Module):
-    def __init__(self):
-        """
-           Your code here.
-           Setup your detection network
-        """
+
+
+#-------------->DETECTOR() UPBLOCK BEGIN
+
+    class UpBlock(torch.nn.Module):
+
+        def __init__(self, n_input, n_output, kernel_size=3, stride=2):
+            super().__init__()
+            self.c1 = torch.nn.ConvTranspose2d(n_input, n_output, kernel_size=kernel_size, padding=kernel_size // 2,
+                                      stride=stride, output_padding=1)
+
+        def forward(self, x):
+            return F.relu(self.c1(x))
+            
+#-------------->DETECTOR  UPBLOCK END
+
+
+
+
+#------------------------------------------------------------------------------>BEGIN CONSTRUCTOR FOR DETECTOR NETWORK
+    def __init__(self, layers=[16, 32, 64, 128], n_output_channels=5, kernel_size=3, use_skip=True):
         super().__init__()
+        self.input_mean = torch.Tensor([0.2788, 0.2657, 0.2629])
+        self.input_std = torch.Tensor([0.2064, 0.1944, 0.2252])
+
+        c = 3
+        self.use_skip = use_skip             #use_skip initially TRUE
+        self.n_conv = len(layers)
+        skip_layer_size = [3] + layers[:-1]  #layers[:-1] is [16, 32, 64], so skip_layer_size is [3, 16, 32, 64]
+        for i, l in enumerate(layers):
+            self.add_module('conv%d' % i, CNNClassifier.Block(c, l, kernel_size, 2))
+            c = l
+        for i, l in list(enumerate(layers))[::-1]:
+            self.add_module('upconv%d' % i, self.UpBlock(c, l, kernel_size, 2))
+            c = l
+            if self.use_skip:
+                c += skip_layer_size[i]
+        self.classifier = torch.nn.Conv2d(c, n_output_channels, 1)
+#------------------------------------------------------------------------------>END CONSTRUCTOR FOR DETECTOR NETWORK
        
 
+
+
+
+
+#------------------------------------------------------------------------------>BEGIN  Detector.FORWARD() METHOD FOR DETECTOR NETWORK
+##=============================================================================>  USES Detector.DETECT() METHOD FOR DETECTION
+
     def forward(self, x):
-        """
-           Your code here.
-           Implement a forward pass through the network, use forward for training,
-           and detect for detection
-        """
-       
+        
+      #Implement a forward pass through the network, use forward for training
+      #and detect for detection       
+
+      print (detect(x))
+      z = (x - self.input_mean[None, :, None, None].to(x.device)) / self.input_std[None, :, None, None].to(x.device)
+      up_activation = []
+      
+      for i in range(self.n_conv):             #in range 4 basically.
+        # Add all the information required for skip connections
+        up_activation.append(z)
+        z = self._modules['conv%d'%i](z)
+
+      for i in reversed(range(self.n_conv)):
+        z = self._modules['upconv%d'%i](z)
+        # Fix the padding
+        z = z[:, :, :up_activation[i].size(2), :up_activation[i].size(3)]
+        # Add the skip connection
+        if self.use_skip:
+          z = torch.cat([z, up_activation[i]], dim=1)
+      return self.classifier(z)
+
+
+
+
+
+
+#------------------------------------------------------------------------------>BEGIN  Detector.DETECT() METHOD FOR DETECTOR NETWORK
+##=============================================================================>  USED BY Detector.FORWARD() METHOD ABOVE FOR DETECTION
+
     def detect(self, image):
         """
            Your code here.
            Implement object detection here.
-           @image: 3 x H x W image
-           @return: Three list of detections [(score, cx, cy, w/2, h/2), ...], one per class,
-                    return no more than 30 detections per image per class. You only need to predict width and height
-                    for extra credit. If you do not predict an object size, return w=0, h=0.
-           Hint: Use extract_peak here
-           Hint: Make sure to return three python lists of tuples of (float, int, int, float, float) and not a pytorch
-                 scalar. Otherwise pytorch might keep a computation graph in the background and your program will run
-                 out of memory.
+           
+           image:           3 x H x W image
+           return:          Three list of detections [(score, cx, cy, w/2, h/2), ...], one per class,
+                            return no more than 30 detections per image per class. You only need to predict width and height
+                            for extra credit. If you do not predict an object size, return w=0, h=0.
+           
+           Hint:  Use extract_peak here
+           Hint:  Make sure to return THREE python lists of tuples of (float, int, int, float, float) and not a pytorch
+                  scalar. Otherwise pytorch might keep a computation graph in the background and your program will run
+                  out of memory.
         """
        
-
+                  #extract_peak(heatmap, max_pool_ks=7, min_score=-5, max_det=100):
+                
+                    
+                    #Extract local maxima (peaks) in a 2d heatmap.
+                    #heatmap:               H x W heatmap containing peaks (similar to your training heatmap)
+                    #max_pool_ks:           Only return points that are larger than a max_pool_ks x max_pool_ks window around the point
+                    #min_score:             Only return peaks greater than min_score
+                    #return:                 List of peaks [(score, cx, cy), ...], where cx, cy are the position of a peak and score is the
+                    #                        heatmap value at the peak. Return no more than max_det peaks per image
+        
+        
+        print(extract_peak(image))
+        return ("Hello World from Detector.Forward()--->detect() ****************OCT 29, 2021")              
+   
 
 
 
