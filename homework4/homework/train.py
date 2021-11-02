e438c2df277f442bff0b96b5fdcfc05396889078
Santos-A-PerezUTexas <saperez@utexas.edu>
2021-11-02 21:15:16 +0000

    Update From Colab

--- homework4/homework/train.py
+++ homework4/homework/train.py
@@ -150,6 +150,9 @@ def train(args):
             print (f'              (LOOP)LOSS  shape is {focal_Loss.shape}')
                         
             print (f'  (LOOP)   Finished making prediction/detection for batch {batch} ')
+            optimizer.zero_grad()
+            focal_Loss.backward()
+            optimizer.step()
 
           
         save_model(model)
