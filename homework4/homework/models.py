12ccdda440ec34be76daf2a6c8ed33c04f46172f
Santos-A-PerezUTexas <saperez@utexas.edu>
2021-11-03 01:51:08 +0000

    Update From Colab

--- homework4/homework/models.py
+++ homework4/homework/models.py
@@ -1,132 +1,9 @@
 import torch
 import torch.nn.functional as F
 
-"""
-NOVEMBER 2, 2021
-https://piazza.com/class/ksjhagmd59d6sg?cid=779
-
-After applying ToTensor() and ToHeatmap(), it will return image, label_for_peak (heatmap), label_for_size. 
-For the model training: peak_loss = BCEWithlogitloss(label_for_peak, model(image))
-Yes, the ToHeatmap() just prepare the labels for us.
-
-Detailly:
-
-
-ToHeatmap() (TRANSFORM) will convert the “boxed” objects, to heatmaps with their center “highlighted”, the centers are 1, 
-the pixels further to the center will have lower values (follow Gaussian distribution). Also it will prepare the size label,
- which has a big circle over the object, the circle has the height and width of the object.
-
-
-Given a center (cx, cy), the height and width will retrieved by label_for_size(B, 0, cx, cy) and label_for_size(B, 1, cx, cy).
-
-
-Yes, you need to train the model (forward), then in the detect(), let the image pass the forward to get the heatmap, 
-then apply extract_peak on the heatmap.
-
-
-
-"""
-
-
-
-
-
-#Oct 22 2021
-
-#torch.nn.functional.max_pool2d(*args, **kwargs)
-#Applies a 2D max pooling over an input signal composed of several input planes.
-#When using the 2D Global Max Pooling block, all of the input channels are combined 
-#into one large pool (i.e., the Horizontal pooling factor x the Vertical pooling factor), 
-#and a single maximum value is computed for each channel.
-#https://www.quora.com/What-is-Max-Pooling-2D
-
-#torch.topk(input, k, dim=None, largest=True, sorted=True, *, out=None)
-#Returns the k largest elements of the given input tensor along a given dimension.
-
 
 
 def extract_peak(heatmap, max_pool_ks=7, min_score=-5, max_det=100):
-    """
-       
-       *The detect and extract peaks  take one image of (3,96,128) at a time right, Not a batch
-       
-       *The peaks in the heatmaps we’re training on are bigger than one pixel 
-       (you can have a look at the training heatmaps by using the log function given in the train.py file)
-        and we want to only extract the center point.
-
-
-       Extract local maxima (peaks) in a 2d heatmap.
-       @heatmap: H x W heatmap containing peaks (similar to your training heatmap)
-       @max_pool_ks: Only return points that are larger than a max_pool_ks x max_pool_ks window around the point
-       @min_score: Only return peaks greater than min_score
-       @return: List of peaks [(score, cx, cy), ...], where cx, cy are the position of a peak and score is the
-                heatmap value at the peak. Return no more than max_det peaks per image
-
-      Use *maxpool2d()* and *topk()*:
-
-      The Heatmap has 3 channels. MaxPool2d operates channel wise and will return 3 channels..  once we *compare* the 
-      maxpool with the heatmap, should  we use a heuristic to combine the channels (eg summation), do we need a 
-      separate convolutional layer to deep learn the right way to  combine channel information, just pass three 
-      separate channels, or something else?
-
-      how to apply peak-extract to a multi-channel heatmap
-          ---------->If you're just working on the extract_peak function, we should be passing one channel at a time to the extract_peak function.
-          ---------->So the "heatmap" input to extract_peak function is just HxW
-          ---------->As described in the starter code, extract_peak() is a function that works for one channel of heatmap (H x W).
-          ---------->In detect(), you will need to let an image (3 x H x W) pass the called forward() and get a 3-channel heatmap (3 x H x W).
-          ---------->Then in detect() you will apply the extract_peak to the 3-channel heatmap three times (by a for loop or list them), then return 3 list of detections.
-
-
-      As noted in the slides, maxpool2d() helps you identify the local maximum by *distributing* the max value to all
-      indexes in a given window. In the provided example, we want to know that 3 is indeed not a local maxima. When 
-      maxpool2d() returns the modified tensor with 5 distributed over the index where 3 was originally located, it 
-      becomes clear that 3 is not greater than or equal to its neighbors.  You need to make use of the information 
-      maxpool2d() provides you with, and generate a new tensor suitable for ***TOPK()***. 
-      You should not pass the raw output of maxpool to topk. The slides also illustrates how you can find the true indexes to
-      generate  the new tensor.
-
-      After max pooling(), how does one pick out the actual maxima from the returned tensor? The only way I can think of is
-      by basically manually doing what maxpool2d() is doing, but that obviously defeats the purpose. I know the next steps are
-       to pass a ***flattened** tensor to *topk()*, but am missing something in the order of operations here.
-               So you have two tensors after max pooling, the pool and the original heat map. There are very simple
-               operations and checks you can do you can *compare* the two to isolate the peaks. I did this comparison before 
-               passing it into *topk()*
-
-
-      **********torch.topk(input, k, dim=None, largest=True, sorted=True, *, out=None)
-                  Returns the k largest elements of the given input tensor along a given dimension.
-      **********torch.nn.MaxPool2d(kernel_size=max_pool_ks=7, stride=None, padding=0, dilation=1, return_indices=True, ceil_mode=False)
-                        **return_indices – if True, will return the max indices along with the outputs. 
-                        **Max pooling expects an order-4 tensor as an input, use heatmap[None,None] to convert the input heatmap to an order-4 tensor.
-                        **Input size (N, C, H, W), output (N, C, H_{out}, W_{out}) 
-
-             1. Heatmap [None, None] to convert the input heatmap to an order-4 tensor.
-             2. Maxpool = Maxpool2d (kernel_size=max_pool_ks=7, return_indices=True) #return indices?
-             3. maxpooled_heatmap =  MaxPool(Heatmap) #identify local maximum by *distributing* max value to all indexes in a given window
-             3. flattened_tensor = Flattenned maxpooled_heatmap
-             4. topk(flattened_tensor, k, dim=None, largest=True, sorted=True, *, out=None)
-             5.  What is k for topk()?
-                      k must <= max_det
-                      k must <=the number of points output by max_pool2d. 
-                      https://piazza.com/class/ksjhagmd59d6sg?cid=256
-
-             6. Where do I use these min_score=-5, max_det=100 ????
-             7. Return no more than max_det peaks per image
-             8. 
-             9. Return:  (score, cx, cy) is a tuple with three numbers
-
-             RETURN (List of peaks [(score, cx, cy), ...], where cx, cy are the position of a peak and score is the heatmap value at the peak. (score, cx, cy) is a tuple with three numbers)
-                        Return no more than max_det peaks per image
-
-           EXAMPLE:
-
-              1.  x = torch.tensor([[0,0,0,0,0], [0,1,2,3,0], [0,4,5,6,0],[0,7,8,9,0],[0,0,0,0,0]])
-              2. Maxpool = torch.nn.MaxPool2d (kernel_size=3)
-              3. x.vew(-1) is the flattened x, but not what we want, we want x[None,None]
-              4. So y = Maxpool(x[None,None]) ---> extract_peaks() takes in a 2-D tensor. Expand the 2-D tensor to a 4-D tensor by using 4d_tensor = 2d_tensor[None, None].
-              5. ***RuntimeError: "max_pool2d" not implemented for 'Long'  (locally when tested)
-    
-    """ 
     return("You have succesfully called extract_peak as follows:  Detector-->Forward()--->Detect()--->extrac_peak")
 
 
@@ -263,14 +140,6 @@ class Detector(torch.nn.Module):
 
     def forward(self, x):
         
-      #Implement a forward pass through the network, use forward for training
-      #and detect for detection  
-      #essentially be the same as HW3 downsample and then upsample back the resolution of the image   
-      #You can use only forward() in training, not detect(). 
-      #You can call detect() optionally for inference/visualization
-      # purposes, but it seems to be "necessary" only in the sense that the grader will evaluate and score it. 
-      #Yeah so detect() is purely for inference. Training portion is very similar to HW3
-
 
       print("MODELS.PY Making a Prediction/Detection NOW, INSIDE FORWARD, OCT 30 2021")
       print (self.detect(x))   #CALLING DETECT() HERE FOR TEST PURPOSES OCT 30 2021          
@@ -292,11 +161,6 @@ class Detector(torch.nn.Module):
         # Add all the information required for skip connections
         up_activation.append(z)
         print(f'            In detector->Forward LOOP Number {i+1} AFTER append()')
-        #AT THIS POINT, GET_ITEM IS CALLED, WHY?  AND PROGRAM CRASHES, OCT 30, 2021
-        #NOV 1, 2021 CRASH HERE AND LINE 119
-        #NOV 1, 2021 CRASH HERE AND LINE 119
-        #NOV 1, 2021 CRASH HERE AND LINE 119
-        #CONVOLUTIONAL LAYER, CLASS CNN's BLOCK, DOES NOT REACH DECON BELOW
         z = self._modules['conv%d'%i](z)   
         
 
@@ -322,41 +186,6 @@ class Detector(torch.nn.Module):
 ##=============================================================================>  USED BY Detector.FORWARD() METHOD ABOVE FOR DETECTION
 
     def detect(self, image):
-        """
-          Implement object detection here.
-           
-           image:           3 x H x W image
-           return:          Three list of detections [(score, cx, cy, w/2, h/2), ...], one list per class (karts, bombs, pickup),
-                            return no more than 30 detections per image per class. Predict width and height
-                            for *extra credit*. If you do not predict an object size, return w=0, h=0.
-           
-           Hint:  Use extract_peak() 
-           Hint:  Return THREE python lists of tuples of (float, int, int, float, float) and not a pytorch
-                  scalar.
-                  
-              The final step of your detector extracts local maxima from each predicted heatmap. Each local maxima corresponds to a positive detection.
-              The function detect() returns a tuple of detections as a list of five numbers per class (i.e., **tuple of three lists): The confidence of the 
-              detection (float, higher means more confident, see evaluation), the x and y location of the object center (center of the predicted bounding box), 
-              and the ***size** of the bounding box (width and height). Your detection function may return up to 100 detections per image, each detection comes with a confidence. 
-              You’ll pay a higher price for getting a high confidence detection wrong. The value of the heatmap at the local maxima (peak score) is a good confidence measure.
-              Use the extract_peak function to find detected objects.
-            
-        """
-           
-                  #ToTheatmaps converts ALL THREE OF THESE DETECTIONS  to peak and size tensors, which are the HEATMAPS????.
-                
-                  #extract_peak(heatmap, max_pool_ks=7, min_score=-5, max_det=100):
-                  #Extract local maxima (peaks) in a 2d heatmap.
-                  #heatmap:               H x W heatmap containing peaks (similar to your training heatmap)
-                  #max_pool_ks:           Only return points that are larger than a max_pool_ks x max_pool_ks window around the point
-                  #min_score:             Only return peaks greater than min_score
-                  #return:                 List of peaks [(score, cx, cy), ...], where cx, cy are the position of a peak and score is the
-                  #heatmap value at the peak. Return no more than max_det peaks per image
-        
-            #Oct 31, 2021
-            #You can call detect() optionally for inference/visualization
-            #purposes, but it seems to be "necessary" only in the sense that the grader will evaluate and score it. 
-            #Yeah so detect() is purely for inference. Training portion is very similar to HW3
 
         
         print(extract_peak(image))
