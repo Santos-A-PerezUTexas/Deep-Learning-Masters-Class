40aaf0fd902faa453e29750f265f368f54f3a278
Santos-A-PerezUTexas <saperez@utexas.edu>
2021-11-04 15:48:49 +0000

    Update From Colab

--- homework4/homework/train.py
+++ homework4/homework/train.py
@@ -97,7 +97,7 @@ def train(args):
     
 
     global_step = 0
-    for epoch in range(1):   #WARNING CHANGE TO args.num_epoch   #WARNING CHANGE TO args.num_epoch
+    for epoch in range(20):   #WARNING CHANGE TO args.num_epoch   #WARNING CHANGE TO args.num_epoch
 
         print(f'***********At the beggining of epoch  {epoch+1}****************')
         model.train()
