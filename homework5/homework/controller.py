71f7b066ced6eecf9a533f959b8e6031bcb79a1c
Santos-A-PerezUTexas <saperez@utexas.edu>
2021-11-14 04:29:27 +0000

    Update From Colab

--- homework5/homework/controller.py
+++ homework5/homework/controller.py
@@ -44,10 +44,23 @@ def control(aim_point, current_vel):
 
     """
     Your code here
+    https://piazza.com/class/ksjhagmd59d6sg?cid=358
+    
+    Hint: Skid if the steering angle is too large.
+    Hint: Target a constant velocity.
+    
+    Hint: Steering and relative aim point use different units. Use the aim point and a tuned scaling factor
+    to select the amount of normalized steering.
+    
+    Hint: Make sure that your controller is able to complete all levels before proceeding to the next part of 
+          the homework because you will use your controller to build the training set for your planner.
+          
     Hint: Use action.acceleration (0..1) to change the velocity. Try targeting a target_velocity (e.g. 20).
     Hint: Use action.brake to True/False to brake (optionally)
+
     Hint: Use action.steer to turn the kart towards the aim_point, clip the steer angle to -1..1
     Hint: You may want to use action.drift=True for wide turns (it will turn faster)
+
     """
 
     return action
