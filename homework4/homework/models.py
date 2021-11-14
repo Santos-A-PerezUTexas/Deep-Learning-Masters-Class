55ad1308c10f39474e31a25f8663bedb9b93acc2
Santos-A-PerezUTexas <saperez@utexas.edu>
2021-11-14 01:34:52 +0000

    Update From Colab

--- homework4/homework/models.py
+++ homework4/homework/models.py
@@ -2,258 +2,136 @@ import torch
 import torch.nn.functional as F
 
 
-#max_det = 100??
-#max_det = 100??
-#max_det = 100??
-
-def get_idx(idx, shape):
+def extract_peak(heatmap, max_pool_ks=7, min_score=-5, max_det=100):
+    """
+       Your code here.
+       Extract local maxima (peaks) in a 2d heatmap.
+       @heatmap: H x W heatmap containing peaks (similar to your training heatmap)
+       @max_pool_ks: Only return points that are larger than a max_pool_ks x max_pool_ks window around the point
+       @min_score: Only return peaks greater than min_score
+       @return: List of peaks [(score, cx, cy), ...], where cx, cy are the position of a peak and score is the
+                heatmap value at the peak. Return no more than max_det peaks per image
+    """
+    max_cls = F.max_pool2d(heatmap[None, None], kernel_size=max_pool_ks, padding=max_pool_ks // 2, stride=1)[0, 0]
+    
+    possible_det = heatmap - (max_cls > heatmap).float() * 1e5
+    
+    if max_det > possible_det.numel():
+        max_det = possible_det.numel()
+        
+        
+    topk_score, topk_idx = torch.topk(possible_det.view(-1), max_det)
     
     
-    res = []
-    N = shape[0]*shape[1]
-    for n in shape:
-        N //= n
-        res.append(idx // N)
-        idx %= N
-    return tuple(res)
-
-def extract_peak(heatmap, max_pool_ks=3, min_score=-5, max_det=30):
-
-    #print ("----------------------EXTRACT PEAK CALLED------------------------")
-
-    detection_list = []
-    k=max_det
-    Maxpool =  torch.nn.MaxPool2d(kernel_size=max_pool_ks, return_indices=False, padding=max_pool_ks//2, stride=1)
+    return [
     
-    heatmap4D = heatmap[None, None]   # reduced=torch.Size([1, 1, 96, 128], heatmap=torch.Size([96, 128])
-    heatmap4D = heatmap4D.to(heatmap.device)
-    maxpooled_heatmap  =  Maxpool(heatmap4D).to(heatmap.device)  
-
-    maxpooled_heatmap = maxpooled_heatmap[0][0]   #torch.Size([96, 128])
-    maxpooled_heatmap = maxpooled_heatmap.to(heatmap.device)
-
-    peak_tensor = torch.where((heatmap==maxpooled_heatmap) & (heatmap>min_score), 1, 0)     #0-1 Heatmap W/ Peaks
-    peak_tensor = peak_tensor.to(heatmap.device)
-    number_of_peaks = torch.count_nonzero(peak_tensor)
-
-    z = torch.zeros(heatmap.shape).to(heatmap.device)
-    z.fill_(-1000000)
-   
-    peaks = torch.where((peak_tensor)==1, heatmap, z).to(heatmap.device)
-    #peaks = torch.where(peaks>min_score, heatmap, z)
-
-    #print(f'sorted peaks top 5 is {sorted(peaks.view(-1))[-5:]}')
-    #print(f'peaks top 5 with topk {torch.topk(peaks.view(-1), 5)[0]}')
-    
-    #topk = torch.topk(peaks.view(-1), k)[0]  #these are the scores
-     
-   
-    
-    if number_of_peaks > k:
-      topk_idx = torch.topk(peaks.view(-1), k)[1].to(heatmap.device)
-    
-    if number_of_peaks <= k:
-      topk_idx = torch.topk(peaks.view(-1), number_of_peaks)[1].to(heatmap.device)
-
-    #cx, cy = get_idx(topk_idx, heatmap.shape)
-    
-    #print(f'Topk returned ---------------{len(topk_idx)} ----------- values')
-    #print(f'Topk  RIGHT HERE---------------{topk_idx.shape} ----------- ')
-    
-    
-    for  i in topk_idx:
-      cx, cy = get_idx(i, heatmap.shape)
-      if peaks[cx][cy] > min_score:
-        detection_list.append((peaks[cx][cy].float(), cx, cy, 0., 0.))
-      
+            (float(s),    int(l) % heatmap.size(1),    int(l) // heatmap.size(1))
+            
+            for s, l in zip(topk_score.cpu(), topk_idx.cpu()) if s > min_score
+            
+            ]
 
 
-    #for i in range(k):
-      #print(heatmap[cx[i]][cy[i]])
-      #if peaks[cx[i]][cy[i]] > min_score:
-       # print(i, peaks[cx[i]][cy[i]],cx[i], cy[i] )
-        #detection_list.append((peaks[cx[i]][cy[i]].float(), cx[i], cy[i],0.,0.))
-      
-
-    #print(f'--------cx is {cx}---------cy is {cy}<-----------------------')
-    #print ("CALLING FOR LOOP------")
-    #print (f'heatmap shape size is {heatmap.shape[0]*heatmap.shape[1]} ')
-    #for i in range (heatmap.shape[0]*heatmap.shape[1]):
-     # if peak_tensor.view(-1)[i]:
-      #    cx, cy=get_idx(i, heatmap.shape)
-       #   if heatmap[cx][cy] > min_score:
-        #    detection_list.append((heatmap[cx][cy], cx, cy,0 ,0))
-    
-    #print ("DETECTION LIST LENGTH AND CONTENTS")
-    #print (len(detection_list))  #should be < max_det
-    #print (detection_list)
-    #print ("SORTED DETECTION LIST, first three entires")
-    #print(sorted(detection_list)[-3:])   
-    #print ("EXTRACT_PEAK()---------------DETECTION LIST LENGTH")
-    #print (len(detection_list))  #should be < max_det
-    #print (detection_list)
-    #print ("SORTED DETECTION LIST LENGTH AND CONTENTS")
-
-
-    return(detection_list)
-
-
-#END----------------------END  extract_peak
-
-        #--------------------------------------BEGIN CNN
-
-class CNNClassifier(torch.nn.Module):
+class Detector(torch.nn.Module):
     class Block(torch.nn.Module):
         def __init__(self, n_input, n_output, kernel_size=3, stride=2):
             super().__init__()
             self.c1 = torch.nn.Conv2d(n_input, n_output, kernel_size=kernel_size, padding=kernel_size // 2,
-                                      stride=stride, bias=False)
-            self.c2 = torch.nn.Conv2d(n_output, n_output, kernel_size=kernel_size, padding=kernel_size // 2, bias=False)
-            self.c3 = torch.nn.Conv2d(n_output, n_output, kernel_size=kernel_size, padding=kernel_size // 2, bias=False)
+                                      stride=stride)
+            self.c2 = torch.nn.Conv2d(n_output, n_output, kernel_size=kernel_size, padding=kernel_size // 2)
+            self.c3 = torch.nn.Conv2d(n_output, n_output, kernel_size=kernel_size, padding=kernel_size // 2)
             self.b1 = torch.nn.BatchNorm2d(n_output)
             self.b2 = torch.nn.BatchNorm2d(n_output)
             self.b3 = torch.nn.BatchNorm2d(n_output)
             self.skip = torch.nn.Conv2d(n_input, n_output, kernel_size=1, stride=stride)
 
         def forward(self, x):
-          return F.relu(self.b3(self.c3(F.relu(self.b2(self.c2(F.relu(self.b1(self.c1(x)))))))) + self.skip(x))
-           
-           
-             #CNN Classifier begins here, the FCN uses ONLY above block.  Nov, 1, 2021
+            return F.relu(self.b3(self.c3(F.relu(self.b2(self.c2(F.relu(self.b1(self.c1(x)))))))) + self.skip(x))
 
-    def __init__(self, layers=[16, 32, 64, 128], n_output_channels=6, kernel_size=3):
-        super().__init__()
-        self.input_mean = torch.Tensor([0.3235, 0.3310, 0.3445])
-        self.input_std = torch.Tensor([0.2533, 0.2224, 0.2483])
-
-        L = []
-        c = 3
-        for l in layers:
-            L.append(self.Block(c, l, kernel_size, 2))   #The Network is composed of a series of blocks w/ three layers each and skip C
-            c = l
-        self.network = torch.nn.Sequential(*L)
-        self.classifier = torch.nn.Linear(c, n_output_channels)    #LINEAR LAYER!!!!!!!!???   Nov 1, 2021
-
-    def forward(self, x):
-        z = self.network((x - self.input_mean[None, :, None, None].to(x.device)) / self.input_std[None, :, None, None].to(x.device))
-        return self.classifier(z.mean(dim=[2, 3]))
-
-
-        #--------------------------------------END CNN
-
-
-
-
-
-
-class Detector(torch.nn.Module):
-
-
-#-------------->DETECTOR() UPBLOCK BEGIN
-
-    class UpBlock(torch.nn.Module):          #DECONVOLUTION
-
+    class UpBlock(torch.nn.Module):
         def __init__(self, n_input, n_output, kernel_size=3, stride=2):
             super().__init__()
             self.c1 = torch.nn.ConvTranspose2d(n_input, n_output, kernel_size=kernel_size, padding=kernel_size // 2,
-                                      stride=stride, output_padding=1)
+                                               stride=stride, output_padding=1)
 
         def forward(self, x):
             return F.relu(self.c1(x))
-            
-#-------------->DETECTOR  UPBLOCK CLASS END 
 
-
-#NOV 2, 2021, CHANGED n_output_channels=5 to 3!!!!!
-
-#------------------------------------------------------------------------------>BEGIN CONSTRUCTOR FOR DETECTOR NETWORK
-    def __init__(self, layers=[16, 32, 64, 128], n_output_channels=3, kernel_size=3, use_skip=True):
+    def __init__(self, layers=[16, 32, 64, 128], n_class=3, kernel_size=3, use_skip=True):
+        """
+           Your code here.
+           Setup your detection network
+        """
         super().__init__()
         self.input_mean = torch.Tensor([0.2788, 0.2657, 0.2629])
         self.input_std = torch.Tensor([0.2064, 0.1944, 0.2252])
 
         c = 3
-        self.use_skip = use_skip             #use_skip initially TRUE
+        self.use_skip = use_skip
         self.n_conv = len(layers)
-        skip_layer_size = [3] + layers[:-1]  #layers[:-1] is [16, 32, 64], so skip_layer_size is [3, 16, 32, 64]
-        
-        #CONVOLUTIONAL LAYERS - CNN BLOCKS
+        skip_layer_size = [3] + layers[:-1]
         for i, l in enumerate(layers):
-            self.add_module('conv%d' % i, CNNClassifier.Block(c, l, kernel_size, 2))   #ONLY USING NON-LINEAR CNN BLOCKS!
+            self.add_module('conv%d' % i, self.Block(c, l, kernel_size, 2))
             c = l
-        
-        #DECONVOLUTIONAL LAYERS
+        # Produce lower res output
         for i, l in list(enumerate(layers))[::-1]:
             self.add_module('upconv%d' % i, self.UpBlock(c, l, kernel_size, 2))
             c = l
             if self.use_skip:
-                #print(f'                         ADDING DECONV SKIP, c is {c}<<<<<<<<<<<<<<<<<<<<')
                 c += skip_layer_size[i]
-                #print(f'                         JUST ADDED DECONV SKIP, c is NOW {c}<<<<<<<<<<<<<<<<<<<<')
+        self.classifier = torch.nn.Conv2d(c, n_class, 1)
+        self.size = torch.nn.Conv2d(c, 2, 1)
 
-      
-        self.classifier = torch.nn.Conv2d(c, n_output_channels, 1)      #MAY NEED TO CHANGE THIS OUTPUT NOV 1, 2021
-#------------------------------------------------------------------------------>END CONSTRUCTOR FOR DETECTOR NETWORK
-       
+    def forward(self, x):
+        """
+           Your code here.
+           Implement a forward pass through the network, use forward for training,
+           and detect for detection
+        """
+        z = (x - self.input_mean[None, :, None, None].to(x.device)) / self.input_std[None, :, None, None].to(x.device)
+        up_activation = []
+        for i in range(self.n_conv):
+            # Add all the information required for skip connections
+            up_activation.append(z)
+            z = self._modules['conv%d' % i](z)
 
+        for i in reversed(range(self.n_conv)):
+            z = self._modules['upconv%d' % i](z)
+            # Fix the padding
+            z = z[:, :, :up_activation[i].size(2), :up_activation[i].size(3)]
+            # Add the skip connection
+            if self.use_skip:
+                z = torch.cat([z, up_activation[i]], dim=1)
+        return self.classifier(z), self.size(z)
 
-
-
-
-#------------------------------------------------------------------------------>BEGIN  Detector.FORWARD() METHOD FOR DETECTOR NETWORK
-##=============================================================================>  USES Detector.DETECT() METHOD FOR DETECTION
-#OCT 30, 2021
-
-    def forward(self, img):
+    def detect(self, image, **kwargs):
+        """
+           Your code here.
+           Implement object detection here.
+           @image: 3 x H x W image
+           @return: Three list of detections [(score, cx, cy, w/2, h/2), ...], one per class,
+                    return no more than 30 detections per image per class. You only need to predict width and height
+                    for extra credit. If you do not predict an object size, return w=0, h=0.
+           Hint: Use extract_peak here
+           Hint: Make sure to return three python lists of tuples of (float, int, int, float, float) and not a pytorch
+                 scalar. Otherwise pytorch might keep a computation graph in the background and your program will run
+                 out of memory.
+        """
+        cls, size = self.forward(image[None])
+        size = size.cpu()
+        return [
         
-     
-      heatmap = (img - self.input_mean[None, :, None, None].to(img.device)) / self.input_std[None, :, None, None].to(img.device)
-      
-      up_activation = []
-      
-    
-      for i in range(self.n_conv):             #in range 4 basically.
-        
-        # Add all the information required for skip connections
-        up_activation.append(heatmap)
-        heatmap = self._modules['conv%d'%i](heatmap)   
-           
-
-      for i in reversed(range(self.n_conv)):
-      
-        heatmap = self._modules['upconv%d'%i](heatmap)
-        # Fix the padding
-        heatmap = heatmap[:, :, :up_activation[i].size(2), :up_activation[i].size(3)]
-        # Add the skip connection
-        if self.use_skip:
-          heatmap = torch.cat([heatmap, up_activation[i]], dim=1)
-      
-          
-      return self.classifier(heatmap)  #returns heatmap ([32, 3, 96, 128])
-
-#ABOVE IS DETECTOR->FORWARD RETURN VALUE heatmap ([32, 3, 96, 128])
-
-
-#------------------------------------------------------------------------------>BEGIN  Detector.DETECT() METHOD FOR DETECTOR NETWORK
-##=============================================================================>  USED BY Detector.FORWARD() METHOD ABOVE FOR DETECTION
-
-    def detect(self, image):
-
-        List_of_detection_lists =[]
-
-        
-        three_channel_heatmap = self.forward(image).to(image.device)
-
-            
-        #heatmap shape is torch.Size([1, 3, 96, 128])
-
-        for i in range(three_channel_heatmap.shape[1]):         
-          List_of_detection_lists.append(extract_peak(three_channel_heatmap[0][i]))
-    
-        
-        return (List_of_detection_lists)              
-   
-
+                 [  
+                    (s, x, y, float(size[0, 0, y, x]), float(size[0, 1, y, x]))
+              
+                        for s, x, y in extract_peak(c, max_det=30, **kwargs) 
+                 
+                 ]
+                 
+                 
+                  for c in cls[0]
+                 
+                 ]
 
 
 def save_model(model):
@@ -269,29 +147,19 @@ def load_model():
     r.load_state_dict(load(path.join(path.dirname(path.abspath(__file__)), 'det.th'), map_location='cpu'))
     return r
 
-#---------------------------------------------------MAIN----------------------------------------------------
 
 if __name__ == '__main__':
-    """
-    Shows detections of your detector
-    """
     from .utils import DetectionSuperTuxDataset
+    dataset = DetectionSuperTuxDataset('dense_data/valid', min_size=0)
     import torchvision.transforms.functional as TF
     from pylab import show, subplots
     import matplotlib.patches as patches
 
-    #print("||||||||||||STEP 1||||||||||   IN MAIN(), GOING TO LOAD DATA and ITERATE, AND CALL DETECTOR() ")
-    dataset = DetectionSuperTuxDataset('dense_data/valid', min_size=0)
-
-
-
     device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
     fig, axs = subplots(3, 4)
     model = load_model().eval().to(device)
-    for i, ax in enumerate(axs.flat):    #called about 11 Times
-        
+    for i, ax in enumerate(axs.flat):
         im, kart, bomb, pickup = dataset[i]
-        #print(f'              MEAN: --->In MAIN(), the mean of the Image just pulled is {im.mean()} ')
         ax.imshow(TF.to_pil_image(im), interpolation=None)
         for k in kart:
             ax.add_patch(
@@ -302,10 +170,9 @@ if __name__ == '__main__':
         for k in pickup:
             ax.add_patch(
                 patches.Rectangle((k[0] - 0.5, k[1] - 0.5), k[2] - k[0], k[3] - k[1], facecolor='none', edgecolor='b'))
-        detections = model.detect(im.to(device))  #****************************************************************<<<<<<
+        detections = model.detect(im.to(device))
         for c in range(3):
             for s, cx, cy, w, h in detections[c]:
                 ax.add_patch(patches.Circle((cx, cy), radius=max(2 + s / 2, 0.1), color='rgb'[c]))
         ax.axis('off')
-    
     show()
--- homework4/homework/train.py
+++ homework4/homework/train.py
@@ -1,147 +1,73 @@
 import torch
 import numpy as np
-import torch.nn as nn
-import torch.nn.functional as F
-from torchvision import models
-from torchsummary import summary
 
-from .models import Detector, CNNClassifier, save_model
-from .utils import load_detection_data, load_dense_data
+from .models import Detector, save_model
+from .utils import load_detection_data
 from . import dense_transforms
 import torch.utils.tensorboard as tb
-DENSE_CLASS_DISTRIBUTION = [0.52683655, 0.02929112, 0.4352989, 0.0044619, 0.00411153]
-
-#NOTE CHANGE THE EPOCHS
-#NOTE CHANGE THE EPOCHS
-#NOTE CHANGE THE EPOCHS
-#NOTE CHANGE THE EPOCHS
-#LOGGER - ARE WE USING CONFUSION MATRIX???
-
-
-class FocalLoss(nn.Module):
-    
-     #below removed Nov 2, 2021
-     #def __init__(self, weight=None, 
-     #            gamma=2., reduction='none'):
-    
-    def __init__(self, weight=None, 
-                 gamma=2.):
-        nn.Module.__init__(self)
-        self.weight = weight
-        self.gamma = gamma
-        #self.reduction = reduction
-        
-    def forward(self, input_tensor, target_tensor):
-
-        print ("IN forward of Focal Loss now")
-        log_prob = F.log_softmax(input_tensor, dim=-1)
-        prob = torch.exp(log_prob)
-        
-        new_input_tensor = ((1 - prob) ** self.gamma) * log_prob
-        print (f'Shape of new_input_tensor is {new_input_tensor.shape}')
-        #print (f'Shape of weights is {self.weight.shape}')
-        print (f'Shape of target_tensor is {target_tensor.shape}')
-        #loss = F.nll_loss(new_input_tensor,  target_tensor, weight=self.weight, reduction = self.reduction).to(input_tensor.device)
-        loss = F.nll_loss(new_input_tensor,  target_tensor.long(), weight=self.weight).to(input_tensor.device) 
- 
-
-        print (f'the shape of loss in focalloss() is {loss.shape}')
-
-        return loss
-
-
-
-
 
 
 def train(args):
-    
     from os import path
-    device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
-
-    model = Detector().to(device)
-    model2 = CNNClassifier().to(device)
-    #print(model)
-    #summary(model, (3,96,128))
-
+    model = Detector()
     train_logger, valid_logger = None, None
-    
     if args.log_dir is not None:
         train_logger = tb.SummaryWriter(path.join(args.log_dir, 'train'), flush_secs=1)
         valid_logger = tb.SummaryWriter(path.join(args.log_dir, 'valid'), flush_secs=1)
 
+    """
+    Your code here, modify your HW3 code
+    """
+    import torch
 
+    device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+
+    model = Detector().to(device)
+    if args.continue_training:
+        model.load_state_dict(torch.load(path.join(path.dirname(path.abspath(__file__)), 'det.th')))
+
+    # optimizer = torch.optim.SGD(model.parameters(), lr=args.learning_rate, momentum=0.9, weight_decay=1e-5)
     optimizer = torch.optim.Adam(model.parameters(), lr=args.learning_rate, weight_decay=1e-5)
-    w = torch.as_tensor(DENSE_CLASS_DISTRIBUTION)**(-args.gamma)
-    w=w.to(device)
-    lossBCE = torch.nn.BCEWithLogitsLoss().to(device)
-    #loss = torch.nn.CrossEntropyLoss(weight=w / w.mean()).to(device)
-    #focal_loss = FocalLoss(weight=w / w.mean()).to(device).to(device)
-    #focal_loss = FocalLoss().to(device).to(device)
-    #NOTE FOCAL LOSS HAS NO WEIGHTS!!
-    
+
     import inspect
     transform = eval(args.transform, {k: v for k, v in inspect.getmembers(dense_transforms) if inspect.isclass(v)})
     train_data = load_detection_data('dense_data/train', num_workers=4, transform=transform)
-    valid_data = load_detection_data('dense_data/valid', num_workers=4)
+
+    det_loss = torch.nn.BCEWithLogitsLoss(reduction='none')
+    size_loss = torch.nn.MSELoss(reduction='none')
 
     global_step = 0
-
-    for epoch in range(args.num_epoch):   #WARNING CHANGE TO args.num_epoch   #WARNING CHANGE TO args.num_epoch
-
-       
+    for epoch in range(args.num_epoch):
         model.train()
-        
-        i_pred = 0
-        batch = 0
 
-        for img, heatmaps, size in train_data:        
-            
-            img, heatmaps, size  = img.to(device), heatmaps.to(device),  size.to(device).long()
-            
-            batch +=1    
-            i_pred += 1
+        for img, gt_det, gt_size in train_data:
+            img, gt_det, gt_size = img.to(device), gt_det.to(device), gt_size.to(device)
 
+            size_w, _ = gt_det.max(dim=1, keepdim=True)
 
-            #print (f'TRAIN() ----->MAKING PREDICTION NUMBER {i_pred+1} WITH detected_peaks=model(img)-----------')
-            #Image shape is torch.Size([32, 3, 96, 128])
-            #peaks shape is torch.Size([32, 3, 96, 128])
-            #size shape is torch.Size([32, 2, 96, 128])            
-           
-            
-            predicted_heatmaps = model(img)
-            heatmap_loss=lossBCE(predicted_heatmaps, heatmaps)
-            
-            #loss_val = loss(detected_heatmaps, heatmaps)
-            #([32, 3, 96, 128]) to  [32, 96, 128]
-            #reduced_heatmaps = heatmaps[:, 0, :, :]
-            #peak_loss= focal_loss(predicted_heatmaps, reduced_heatmaps)   #peaks or reduced_peaks
-            #loss = torch.nn.CrossEntropyLoss(weight=w / w.mean()).to(device)
-            #lossBCE = FocalLoss()
-            
+            det, size = model(img)
+            # Continuous version of focal loss
+            p_det = torch.sigmoid(det * (1-2*gt_det))
+            det_loss_val = (det_loss(det, gt_det)*p_det).mean() / p_det.mean()
+            size_loss_val = (size_w * size_loss(size, gt_size)).mean() / size_w.mean()
+            loss_val = det_loss_val + size_loss_val * args.size_weight
 
-             
-
-            print(f'------------>Curent Loss is {heatmap_loss}')
             if train_logger is not None and global_step % 100 == 0:
-                log(train_logger, img, label, logit, global_step)
-            if train_logger is not None:
-                train_logger.add_scalar('loss', loss_val, global_step)
+                log(train_logger, img, gt_det, det, global_step)
 
+            if train_logger is not None:
+                train_logger.add_scalar('det_loss', det_loss_val, global_step)
+                train_logger.add_scalar('size_loss', size_loss_val, global_step)
+                train_logger.add_scalar('loss', loss_val, global_step)
             optimizer.zero_grad()
-            heatmap_loss.backward()
+            loss_val.backward()
             optimizer.step()
             global_step += 1
 
-        model.eval()
-
+        if valid_logger is None or train_logger is None:
+            print('epoch %-3d' %
+                  (epoch))
         save_model(model)
-        print("DONE")
-
-  
-
-#--------------------------------------END TRAIN
-
 
 def log(logger, imgs, gt_det, det, global_step):
     """
@@ -155,59 +81,20 @@ def log(logger, imgs, gt_det, det, global_step):
     logger.add_images('label', gt_det[:16], global_step)
     logger.add_images('pred', torch.sigmoid(det[:16]), global_step)
 
+
 if __name__ == '__main__':
     import argparse
 
     parser = argparse.ArgumentParser()
 
     parser.add_argument('--log_dir')
-    parser.add_argument('-n', '--num_epoch', type=int, default=20)
+    # Put custom arguments here
+    parser.add_argument('-n', '--num_epoch', type=int, default=120)
     parser.add_argument('-lr', '--learning_rate', type=float, default=1e-3)
-    parser.add_argument('-g', '--gamma', type=float, default=0, help="class dependent weight for cross entropy")
     parser.add_argument('-c', '--continue_training', action='store_true')
     parser.add_argument('-t', '--transform',
-                        default='Compose([ColorJitter(0.9, 0.9, 0.9, 0.1), RandomHorizontalFlip(), ToTensor(), ToHeatmap()])')
-    #parser.add_argument('-t', '--transform',
-     #                   default='Compose([ColorJitter(0.9, 0.9, 0.9, 0.1), RandomHorizontalFlip(), ToTensor()])')
+                        default='Compose([ColorJitter(0.9, 0.9, 0.9, 0.1), RandomHorizontalFlip(), ToTensor(), ToHeatmap(2)])')
+    parser.add_argument('-w', '--size-weight', type=float, default=0.01)
 
     args = parser.parse_args()
     train(args)
-
-
-    """
-      if train_logger is not None and global_step % 100 == 0:
-                log(train_logger, img, label, logit, global_step)
-
-            if train_logger is not None:
-                train_logger.add_scalar('loss', loss_val, global_step)
-            conf.add(logit.argmax(1), label)
-
-            optimizer.zero_grad()
-            loss_val.backward()
-            optimizer.step()
-            global_step += 1
-
-        if train_logger:
-            train_logger.add_scalar('global_accuracy', conf.global_accuracy, global_step)
-            train_logger.add_scalar('average_accuracy', conf.average_accuracy, global_step)
-            train_logger.add_scalar('iou', conf.iou, global_step)
-
-        model.eval()
-        val_conf = ConfusionMatrix()
-        for img, label in valid_data:
-            img, label = img.to(device), label.to(device).long()
-            logit = model(img)
-            val_conf.add(logit.argmax(1), label)
-
-        if valid_logger is not None:
-            log(valid_logger, img, label, logit, global_step)
-
-        if valid_logger:
-            valid_logger.add_scalar('global_accuracy', val_conf.global_accuracy, global_step)
-            valid_logger.add_scalar('average_accuracy', val_conf.average_accuracy, global_step)
-            valid_logger.add_scalar('iou', val_conf.iou, global_step)
-
-        if valid_logger is None or train_logger is None:
-            print('epoch %-3d \t acc = %0.3f \t val acc = %0.3f \t iou = %0.3f \t val iou = %0.3f' %
-                  (epoch, conf.global_accuracy, val_conf.global_accuracy, conf.iou, val_conf.iou))
-    """
