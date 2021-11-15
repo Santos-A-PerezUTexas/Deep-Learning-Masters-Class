476208eca997f049d26afb4add7cad98c8b1c7ad
Santos-A-PerezUTexas <saperez@utexas.edu>
2021-11-15 16:45:19 +0000

    Update From Colab

--- homework5/homework/planner.py
+++ homework5/homework/planner.py
@@ -13,23 +13,88 @@ def spatial_argmax(logit):
                         (weights.sum(2) * torch.linspace(-1, 1, logit.size(1)).to(logit.device)[None]).sum(1)), 1)
 
 
-class Planner(torch.nn.Module):
-    def __init__(self):
-        super().__init__()
 
-        """
-        Your code here
-        """
-        raise NotImplementedError('Planner.__init__')
+
+
+
+class Planner(torch.nn.Module):
+
+
+    class Block(torch.nn.Module):
+        def __init__(self, n_input, n_output, kernel_size=3, stride=2):
+            super().__init__()
+            self.c1 = torch.nn.Conv2d(n_input, n_output, kernel_size=kernel_size, padding=kernel_size // 2,
+                                      stride=stride)
+            self.c2 = torch.nn.Conv2d(n_output, n_output, kernel_size=kernel_size, padding=kernel_size // 2)
+            self.c3 = torch.nn.Conv2d(n_output, n_output, kernel_size=kernel_size, padding=kernel_size // 2)
+            self.b1 = torch.nn.BatchNorm2d(n_output)
+            self.b2 = torch.nn.BatchNorm2d(n_output)
+            self.b3 = torch.nn.BatchNorm2d(n_output)
+            self.skip = torch.nn.Conv2d(n_input, n_output, kernel_size=1, stride=stride)
+
+        def forward(self, x):
+            return F.relu(self.b3(self.c3(F.relu(self.b2(self.c2(F.relu(self.b1(self.c1(x)))))))) + self.skip(x))
+
+
+    class UpBlock(torch.nn.Module):
+        def __init__(self, n_input, n_output, kernel_size=3, stride=2):
+            super().__init__()
+            self.c1 = torch.nn.ConvTranspose2d(n_input, n_output, kernel_size=kernel_size, padding=kernel_size // 2,
+                                               stride=stride, output_padding=1)
+
+        def forward(self, x):
+            return F.relu(self.c1(x))
+
+
+
+
+    def __init__(self, layers=[16, 32, 64, 128], n_class=3, kernel_size=3, use_skip=True):
+        
+        super().__init__()
+        self.input_mean = torch.Tensor([0.2788, 0.2657, 0.2629])
+        self.input_std = torch.Tensor([0.2064, 0.1944, 0.2252])
+
+        c = 3
+        self.use_skip = use_skip
+        self.n_conv = len(layers)
+        skip_layer_size = [3] + layers[:-1]
+        for i, l in enumerate(layers):
+            self.add_module('conv%d' % i, self.Block(c, l, kernel_size, 2))
+            c = l
+        # Produce lower res output
+        for i, l in list(enumerate(layers))[::-1]:
+            self.add_module('upconv%d' % i, self.UpBlock(c, l, kernel_size, 2))
+            c = l
+            if self.use_skip:
+                c += skip_layer_size[i]
+        self.classifier = torch.nn.Conv2d(c, n_class, 1)
+        self.size = torch.nn.Conv2d(c, 2, 1)
+
+
 
     def forward(self, img):
+
+
+
         """
-        Your code here
         Predict the aim point in image coordinate, given the supertuxkart image
         @img: (B,3,96,128)
         return (B,2)
+        
+        Use:  spatial_argmax(heatmap or logit), which returns return: A tensor of size BS x 2
+        the soft-argmax in normalized coordinates (-1 .. 1)
+
+        Your planner model is a torch.nn.Module that takes as input an image tensor and outputs
+         the aiming point in image coordinates (x:0..127, y:0..95). We recommend using an encoder-decoder
+         structure to predict a heatmap and extract the peak using a spatial argmax layer 
+
         """
-        raise NotImplementedError("Planner.forward")
+
+
+
+
+
+
 
 
 def save_model(model):
