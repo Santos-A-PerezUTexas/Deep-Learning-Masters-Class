2768b9f2bbabe4e5246a1a7a9ba16af1cb3640a2
Santos-A-PerezUTexas <saperez@utexas.edu>
2021-11-15 20:32:52 +0000

    Update From Colab

--- homework5/homework/train.py
+++ homework5/homework/train.py
@@ -35,7 +35,11 @@ def train(args):
     MSEloss = torch.nn.MSELoss(reduction='none')
 
     global_step = 0
+    loss_val = 0
+    
     for epoch in range(args.num_epoch):
+        
+        print(loss_val)
         model.train()
 
         for img,coordinates in train_data:
@@ -92,7 +96,7 @@ if __name__ == '__main__':
 
     parser.add_argument('--log_dir')
     # Put custom arguments here
-    parser.add_argument('-n', '--num_epoch', type=int, default=120)
+    parser.add_argument('-n', '--num_epoch', type=int, default=20)
     parser.add_argument('-lr', '--learning_rate', type=float, default=1e-3)
     parser.add_argument('-c', '--continue_training', action='store_true')
     parser.add_argument('-t', '--transform',
