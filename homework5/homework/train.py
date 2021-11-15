44d762c3c5238281366f8edc92d1127c7280f03d
Santos-A-PerezUTexas <saperez@utexas.edu>
2021-11-15 17:00:18 +0000

    Update From Colab

--- homework5/homework/train.py
+++ homework5/homework/train.py
@@ -16,8 +16,58 @@ def train(args):
     Your code here, modify your HW4 code
     Hint: Use the log function below to debug and visualize your model
     """
+    import torch
 
-    save_model(model)
+    device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+
+    model = Detector().to(device)
+    if args.continue_training:
+        model.load_state_dict(torch.load(path.join(path.dirname(path.abspath(__file__)), 'det.th')))
+
+    # optimizer = torch.optim.SGD(model.parameters(), lr=args.learning_rate, momentum=0.9, weight_decay=1e-5)
+    optimizer = torch.optim.Adam(model.parameters(), lr=args.learning_rate, weight_decay=1e-5)
+
+    import inspect
+    transform = eval(args.transform, {k: v for k, v in inspect.getmembers(dense_transforms) if inspect.isclass(v)})
+    train_data = load_detection_data('dense_data/train', num_workers=4, transform=transform)
+
+    det_loss = torch.nn.BCEWithLogitsLoss(reduction='none')
+    size_loss = torch.nn.MSELoss(reduction='none')
+
+    global_step = 0
+    for epoch in range(args.num_epoch):
+        model.train()
+
+        for img, gt_det, gt_size in train_data:
+            img, gt_det, gt_size = img.to(device), gt_det.to(device), gt_size.to(device)
+
+            size_w, _ = gt_det.max(dim=1, keepdim=True)
+
+            det, size = model(img)
+            # Continuous version of focal loss
+            p_det = torch.sigmoid(det * (1-2*gt_det))
+            det_loss_val = (det_loss(det, gt_det)*p_det).mean() / p_det.mean()
+            size_loss_val = (size_w * size_loss(size, gt_size)).mean() / size_w.mean()
+            loss_val = det_loss_val + size_loss_val * args.size_weight
+
+            if train_logger is not None and global_step % 100 == 0:
+                log(train_logger, img, gt_det, det, global_step)
+
+            if train_logger is not None:
+                train_logger.add_scalar('det_loss', det_loss_val, global_step)
+                train_logger.add_scalar('size_loss', size_loss_val, global_step)
+                train_logger.add_scalar('loss', loss_val, global_step)
+            optimizer.zero_grad()
+            loss_val.backward()
+            optimizer.step()
+            global_step += 1
+
+        if valid_logger is None or train_logger is None:
+            print('epoch %-3d' %
+                  (epoch))
+        save_model(model)
+
+    
 
 def log(logger, img, label, pred, global_step):
     """
@@ -44,6 +94,12 @@ if __name__ == '__main__':
 
     parser.add_argument('--log_dir')
     # Put custom arguments here
+    parser.add_argument('-n', '--num_epoch', type=int, default=120)
+    parser.add_argument('-lr', '--learning_rate', type=float, default=1e-3)
+    parser.add_argument('-c', '--continue_training', action='store_true')
+    parser.add_argument('-t', '--transform',
+                        default='Compose([ColorJitter(0.9, 0.9, 0.9, 0.1), RandomHorizontalFlip(), ToTensor(), ToHeatmap(2)])')
+    parser.add_argument('-w', '--size-weight', type=float, default=0.01)
 
     args = parser.parse_args()
     train(args)
