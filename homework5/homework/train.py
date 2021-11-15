3a613a55299a1e90df938255f793e0b24b226e8f
Santos-A-PerezUTexas <saperez@utexas.edu>
2021-11-15 18:52:08 +0000

    Update From Colab

--- homework5/homework/train.py
+++ homework5/homework/train.py
@@ -20,7 +20,7 @@ def train(args):
 
     device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
 
-    model = Detector().to(device)
+    model = Planner().to(device)
     if args.continue_training:
         model.load_state_dict(torch.load(path.join(path.dirname(path.abspath(__file__)), 'det.th')))
 
@@ -29,26 +29,22 @@ def train(args):
 
     import inspect
     transform = eval(args.transform, {k: v for k, v in inspect.getmembers(dense_transforms) if inspect.isclass(v)})
-    train_data = load_detection_data('dense_data/train', num_workers=4, transform=transform)
+    train_data = load_data(transform=transform)
 
-    det_loss = torch.nn.BCEWithLogitsLoss(reduction='none')
-    size_loss = torch.nn.MSELoss(reduction='none')
+    
+    MSEloss = torch.nn.MSELoss(reduction='none')
 
     global_step = 0
     for epoch in range(args.num_epoch):
         model.train()
 
-        for img, gt_det, gt_size in train_data:
-            img, gt_det, gt_size = img.to(device), gt_det.to(device), gt_size.to(device)
+        for img,coordinates in train_data:
+            img, coordinates = img.to(device), coordinates.to(device)
 
-            size_w, _ = gt_det.max(dim=1, keepdim=True)
-
-            det, size = model(img)
+            prediction = model(img)
             # Continuous version of focal loss
-            p_det = torch.sigmoid(det * (1-2*gt_det))
-            det_loss_val = (det_loss(det, gt_det)*p_det).mean() / p_det.mean()
-            size_loss_val = (size_w * size_loss(size, gt_size)).mean() / size_w.mean()
-            loss_val = det_loss_val + size_loss_val * args.size_weight
+            loss_val = MSEloss(prediction, coordinates)
+            
 
             if train_logger is not None and global_step % 100 == 0:
                 log(train_logger, img, gt_det, det, global_step)
@@ -98,7 +94,7 @@ if __name__ == '__main__':
     parser.add_argument('-lr', '--learning_rate', type=float, default=1e-3)
     parser.add_argument('-c', '--continue_training', action='store_true')
     parser.add_argument('-t', '--transform',
-                        default='Compose([ColorJitter(0.9, 0.9, 0.9, 0.1), RandomHorizontalFlip(), ToTensor(), ToHeatmap(2)])')
+                        default='Compose([ColorJitter(0.9, 0.9, 0.9, 0.1), RandomHorizontalFlip(), ToTensor()])')
     parser.add_argument('-w', '--size-weight', type=float, default=0.01)
 
     args = parser.parse_args()
