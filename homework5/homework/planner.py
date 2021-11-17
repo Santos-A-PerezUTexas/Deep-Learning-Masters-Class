b8eb496499c711326321a975d3433e44849b4562
Santos-A-PerezUTexas <saperez@utexas.edu>
2021-11-17 02:01:06 +0000

    Update From Colab

--- homework5/homework/planner.py
+++ homework5/homework/planner.py
@@ -12,42 +12,6 @@ def spatial_argmax(logit):
     return torch.stack(((weights.sum(1) * torch.linspace(-1, 1, logit.size(2)).to(logit.device)[None]).sum(1),
                         (weights.sum(2) * torch.linspace(-1, 1, logit.size(1)).to(logit.device)[None]).sum(1)), 1)
 
-
-
-
-class CNNClassifier(torch.nn.Module):
-    class Block(torch.nn.Module):
-        def __init__(self, n_input, n_output, kernel_size=3, stride=2):
-            super().__init__()
-            self.c1 = torch.nn.Conv2d(n_input, n_output, kernel_size=kernel_size, padding=kernel_size // 2,
-                                      stride=stride, bias=False)
-            self.c2 = torch.nn.Conv2d(n_output, n_output, kernel_size=kernel_size, padding=kernel_size // 2, bias=False)
-            self.c3 = torch.nn.Conv2d(n_output, n_output, kernel_size=kernel_size, padding=kernel_size // 2, bias=False)
-            self.b1 = torch.nn.BatchNorm2d(n_output)
-            self.b2 = torch.nn.BatchNorm2d(n_output)
-            self.b3 = torch.nn.BatchNorm2d(n_output)
-            self.skip = torch.nn.Conv2d(n_input, n_output, kernel_size=1, stride=stride)
-
-        def forward(self, x):
-            return F.relu(self.b3(self.c3(F.relu(self.b2(self.c2(F.relu(self.b1(self.c1(x)))))))) + self.skip(x))
-
-    def __init__(self, layers=[16, 32, 64, 128], n_output_channels=6, kernel_size=3):
-        super().__init__()
-        self.input_mean = torch.Tensor([0.3235, 0.3310, 0.3445])
-        self.input_std = torch.Tensor([0.2533, 0.2224, 0.2483])
-
-        L = []
-        c = 3
-        for l in layers:
-            L.append(self.Block(c, l, kernel_size, 2))
-            c = l
-        self.network = torch.nn.Sequential(*L)
-        self.classifier = torch.nn.Linear(c, n_output_channels)
-
-    def forward(self, x):
-        z = self.network((x - self.input_mean[None, :, None, None].to(x.device)) / self.input_std[None, :, None, None].to(x.device))
-        return self.classifier(z.mean(dim=[2, 3]))
-
 class Planner(torch.nn.Module):
 
 
@@ -90,6 +54,13 @@ class Planner(torch.nn.Module):
     def forward(self, img):
     
      
+
+
+      #ADD A SKIP CONNECTION
+      #ADD A SKIP CONNECTION
+      #ADD A SKIP CONNECTION
+      #ADD A SKIP CONNECTION
+      
       #print(f'1    img shape is {img.shape}')
       
       out = self.layer1(img)
