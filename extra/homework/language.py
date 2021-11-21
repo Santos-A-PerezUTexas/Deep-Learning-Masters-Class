97589899a9025fad4695b7f5846fd1a47cd64261
Santos-A-PerezUTexas <saperez@utexas.edu>
2021-11-21 16:06:52 +0000

    Update From Colab

--- extra/homework/language.py
+++ extra/homework/language.py
@@ -8,65 +8,11 @@ from .utils import one_hot
 
 def log_likelihood(model: LanguageModel, 
                    some_text: str):
-    """
-    
-
-    NOV 19:  https://piazza.com/class/ksjhagmd59d6sg?cid=1075
-    The log-likelihood is a single number, predict_all output an array of [28, n+1]. For a *single* 28-dim vector, 
-    you will need only one of them that represents the current character.
-          
-    
-    
-    NOV 15: https://piazza.com/class/ksjhagmd59d6sg?cid=1033
-
-
-
-
    
-    INSTRUCTIONS:
-
-    This function takes a string as input and returns the log probability 
-    of that string under the current language model. Test your implementation using the Bigram 
-    or AdjacentLanguageModel.
-    
-    https://stackoverflow.com/questions/42817834/understanding-maximum-likelihood-in-nlp
-    https://machinelearningmastery.com/what-is-maximum-likelihood-estimation-in-machine-learning/
-    It is common in optimization problems to prefer to minimize the cost function, rather than to maximize it.
-    Therefore, the negative of the log-likelihood function is used, referred to generally as a
-    Negative Log-Likelihood (NLL) function.
-    
-    https://moonbooks.org/Articles/How-to-calculate-a-log-likelihood-in-python-example-with-a-normal-distribution-/
-    
-    
-    
-    Hint: Remember that the language model can take an empty string as input
-
-    Hint: Recall that LanguageModel.predict_all returns the log probabilities of the next characters for all substrings.
-
-    Hint: The log-likelihood is the sum of all individual likelihoods, not the average
-
-    :param model: A LanguageModel
-    :param some_text:
-    :return: float
-    """
-    string = ""
-
-    if some_text:                        #remove this for 20 points!
-     string = some_text                 #remove this for 20 points!
-
-    #print(f'Nov 19 2021 --------------- string len shape is {len(string)}')
-    logit = model.predict_all(string)[:, :-1]
-    v,i = torch.max(logit, dim=0)
-    
-    
-    #print(f'Nov 19 2021 --------------- logit shape is {logit.shape}')
-
-    LL = v.sum()
-
-    print(f'------>>>>>>>>>LL for {string} is {LL} ')
-
-    return LL
-    
+    text = utils.one_hot(some_text)
+    predict_all = model.predict_all(some_text)[:, 0:len(some_text)]
+    likelihoods = torch.mm(predict_all.t(), text)
+    return sum(likelihoods.diag()) 
 
 
 def sample_random(model: LanguageModel, 
@@ -137,79 +83,55 @@ def beam_search(model: LanguageModel,
                 n_results: int = 10, 
                 max_length: int = 100, 
                 average_log_likelihood: bool = False):
-    """
-    
-    Nov 19 2021
+   
+# heap will sort by first element in tuple
+    heap = TopNHeap(beam_size)
+    term_heap = TopNHeap(n_results)
+    visited = set()
 
+    # initialize heap
+    likelihoods = model.predict_next("")
+    for i, l in enumerate(likelihoods):
+        visited.add(utils.vocab[i])
+        if utils.vocab[i] == '.':
+            term_heap.add(( l, utils.vocab[i]) )
+        else:
+            heap.add( (l, utils.vocab[i]) )
 
-    https://piazza.com/class/ksjhagmd59d6sg?cid=1086
+    iters = 0
+    while iters < 50:
 
-    Generate several sentences and try to find the top n sentences with highest log_likelihood.
-    But since we have 28 choices for up to max_length characters, do we need to calculate  all
-    the choices and then feed into the TopNHeap list?    
+        for curr_l, curr_s in heap.elements:
+            likelihoods = model.predict_next(curr_s)
 
+            for i, l in enumerate(likelihoods):
+                new_s = curr_s + utils.vocab[i]
+                if average_log_likelihood:
+                    new_l = log_likelihood(model, new_s) / len(new_s)
+                else:
+                    new_l = curr_l + l
 
+                if new_s not in visited:
+                    visited.add(new_s)
+                    # add to terminated heap
+                    if new_s[-1] == '.' or len(new_s) > max_length:
+                        term_heap.add( (new_l, new_s) )
+                    else:
+                        heap.add( (new_l, new_s) )
+        iters += 1
 
-    Use beam search for find the highest likelihood generations, such that:
-      * No two returned sentences are the same
-      * the `log_likelihood` of each returned sentence is as large as possible
+    # sort and return
+    sort_list = []
+    for i in range(len(term_heap.elements)):
+        sort_list.append(term_heap.elements[i])
+    #sort_list.sort(reverse=True)
+    sort_list.sort()
 
-    Each column is a slot, there are up to max_legth such slots
-    Rows are 0-27,  tells you how probable each letter is in each slot (column)
-
-
-    slot:1   2  3   4
-
-    letter
-
-    a    .5  .2  .1  .9 
-    b    .7  .4   0  .2
-    c    .1  .6  .5  .4
-
-    These are the highest values for the slots:
-
-        1   2   3   4             1   2   3   4
-       .7  .6  .5 .9              b   c   c   a    = sentence
-
-    :param model: A LanguageModel
-    :param beam_size: The size of the beam in beam search (number of sentences to keep around)
-    :param n_results: The number of results to return
-    :param max_length: The maximum sentence length
-    :param average_log_likelihood: Pick the best beams according to the average log-likelihood, not the sum
-                                   This option favors longer strings.
-    :return: A list of strings of size n_results
-    """
-    vocab = string.ascii_lowercase + ' .'
-    
-    print (f'Inside of beam_search, returning list of strings of size n_results size: {n_results}')
-    
-    stringD = ""
-    my_list = []
-    my_heap = TopNHeap(beam_size)
-    
-    for i in range (n_results):
-      for i in range (max_length):
-        prob_next = model.predict_next(stringD)
-        prob_all =  model.predict_all(stringD)
-        max_i = torch.argmax(model.predict_next(stringD))
-        stringD = stringD + vocab[max_i]
-        my_heap.add(prob_next[max_i])   #MY HEAP HEAP
-        if vocab[max_i] == '.':
-          break
-      
-      my_list.append(stringD)
-    
-    
-    #print(f'THE HEAP----------------> {my_heap.elements}')
-    #print (max_length)
-
-    #print ("-----------THE LIST----------------------------------------")
-    #print (my_list)
-    
-
-    return (my_list)
-
-
+    return_list = []
+    for l, s in sort_list:
+        return_list.append(s)
+    print(return_list)
+    return return_list
 if __name__ == "__main__":
     """
       Some test code.
