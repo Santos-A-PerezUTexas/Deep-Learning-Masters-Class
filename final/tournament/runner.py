cd6e8a0db874b8d43796a7749e0598d9ccde258b
Santos-A-PerezUTexas <saperez@utexas.edu>
2021-12-01 19:53:50 +0000

    Update From Colab

--- final/tournament/runner.py
+++ final/tournament/runner.py
@@ -228,12 +228,12 @@ class Match:
             #test
             # Have each team produce actions (in parallel)
             if t1_type == 'image':
-                team1_actions_delayed = self._r(team1.act)(team1_state, team1_images)
+                team1_actions_delayed = self._r(team1.act)(team1_state, team1_images, soccer_state)
             else:
                 team1_actions_delayed = self._r(team1.act)(team1_state, team2_state, soccer_state)
 
             if t2_type == 'image':
-                team2_actions_delayed = self._r(team2.act)(team2_state, team2_images)
+                team2_actions_delayed = self._r(team2.act)(team2_state, team2_images, soccer_state)
             else:
                 team2_actions_delayed = self._r(team2.act)(team2_state, team1_state, soccer_state)
 
