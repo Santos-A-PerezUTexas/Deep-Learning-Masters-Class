965298d126a7572816445af5bf6ee3cfb447bc21
Santos-A-PerezUTexas <saperez@utexas.edu>
2021-12-03 01:24:20 +0000

    Update From Colab

--- extra/homework/models.py
+++ extra/homework/models.py
@@ -251,8 +251,10 @@ class TCN(torch.nn.Module, LanguageModel):     #MY WARNING:  TCN in example DOES
 
         """
         
-        one_hotx = one_hot(some_text)[:, :-1]
+        #one_hotx = one_hot(some_text)[:, :-1]
 
+        one_hotx = one_hot(some_text)
+        
         print (f'in predict_all, sometext is {some_text}')
         print (f'in predict all one_hotx shape is {one_hotx.shape}')
 
