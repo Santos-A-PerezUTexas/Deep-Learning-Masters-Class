9234d431bd958d41d91f4ce69cd930616a7ffffd
Santos A. Perez <89759017+Santos-A-PerezUTexas@users.noreply.github.com>
2021-12-08 13:53:15 -0500

    Update planner.py

--- final/image_agent/planner.py
+++ final/image_agent/planner.py
@@ -1,112 +1,48 @@
+
 import torch
 import torch.nn.functional as F
 
 
+
 def spatial_argmax(logit):
-    """
-    Compute the soft-argmax of a heatmap
-    :param logit: A tensor of size BS x H x W
-    :return: A tensor of size BS x 2 the soft-argmax in normalized coordinates (-1 .. 1)
-    """
+    
     weights = F.softmax(logit.view(logit.size(0), -1), dim=-1).view_as(logit)
     return torch.stack(((weights.sum(1) * torch.linspace(-1, 1, logit.size(2)).to(logit.device)[None]).sum(1),
                         (weights.sum(2) * torch.linspace(-1, 1, logit.size(1)).to(logit.device)[None]).sum(1)), 1)
-
+    
+                        
 class Planner(torch.nn.Module):
-    class Block(torch.nn.Module):
-        def __init__(self, n_input, n_output, kernel_size=3, stride=2):
-            super().__init__()
-            self.c1 = torch.nn.Conv2d(n_input, n_output, kernel_size=kernel_size, padding=kernel_size // 2,
-                                      stride=stride)
-            self.c2 = torch.nn.Conv2d(n_output, n_output, kernel_size=kernel_size, padding=kernel_size // 2)
-            self.c3 = torch.nn.Conv2d(n_output, n_output, kernel_size=kernel_size, padding=kernel_size // 2)
-            self.b1 = torch.nn.BatchNorm2d(n_output)
-            self.b2 = torch.nn.BatchNorm2d(n_output)
-            self.b3 = torch.nn.BatchNorm2d(n_output)
-            self.skip = torch.nn.Conv2d(n_input, n_output, kernel_size=1, stride=stride)
-
-        def forward(self, x):
-          
-            output = self.b1(self.c1(x))
-            #output = F.relu(output) 
-            #output = F.relu(self.b2(self.c2(output)))
-            output = self.b3(self.c3(output))
-            #output = F.relu(output + self.skip(x)) 
-
-            return F.relu(output)  #remove Relu if use skip!
-
-    class UpBlock(torch.nn.Module):
-        def __init__(self, n_input, n_output, kernel_size=3, stride=2):
-            super().__init__()
-            self.c1 = torch.nn.ConvTranspose2d(n_input, n_output, kernel_size=kernel_size, padding=kernel_size // 2,
-                                               stride=stride, output_padding=1)
-
-        def forward(self, x):
-            return F.relu(self.c1(x))
-
-    def __init__(self, layers=[16, 32, 32], n_class=3, kernel_size=3, use_skip=False):
-        """
-           Your code here.
-           Setup your detection network
-        """
+    def __init__(self, channels=[16, 32, 64, 128]):
         super().__init__()
-        self.input_mean = torch.Tensor([0.2788, 0.2657, 0.2629])
-        self.input_std = torch.Tensor([0.2064, 0.1944, 0.2252])
 
-        c = 3
-        self.use_skip = use_skip
-        self.n_conv = len(layers)
-        skip_layer_size = [3] + layers[:-1]
-        for i, l in enumerate(layers):
-            self.add_module('conv%d' % i, self.Block(c, l, kernel_size, 2))
-            c = l
-        # Produce lower res output
-        for i, l in list(enumerate(layers))[::-1]:
-            self.add_module('upconv%d' % i, self.UpBlock(c, l, kernel_size, 2))
-            c = l
-            if self.use_skip:
-                c += skip_layer_size[i]
+        conv_block = lambda c, h: [torch.nn.BatchNorm2d(h), torch.nn.Conv2d(h, c, 7, 2, 3), torch.nn.ReLU(True)]
+        upconv_block = lambda c, h: [torch.nn.BatchNorm2d(h), torch.nn.ConvTranspose2d(h, c, 4, 2, 1),
+                                     torch.nn.ReLU(True)]
+
+        h, _conv, _upconv = 3, [], []
+        for c in channels:
+            _conv += conv_block(c, h)
+            h = c
+
+        for c in channels[:-3:-1]:
+            _upconv += upconv_block(c, h)
+            h = c
+
+        _upconv += [torch.nn.BatchNorm2d(h), torch.nn.Conv2d(h, 1, 1, 1, 0)]
+
+        self._conv = torch.nn.Sequential(*_conv)
+        self._upconv = torch.nn.Sequential(*_upconv)   
+        self._mean = torch.FloatTensor([0.4519, 0.5590, 0.6204])
+        self._std = torch.FloatTensor([0.0012, 0.0018, 0.0020])
+
+    def forward(self, img):
         
-        #self.classifier = torch.nn.Conv2d(c, n_class, 1)
-        #self.size = torch.nn.Conv2d(c, 2, 1)
+        img = (img - self._mean[None, :, None, None].to(img.device)) / self._std[None, :, None, None].to(img.device)
+        h = self._conv(img)
+        x = self._upconv(h)
+        return (1 + spatial_argmax(x.squeeze(1))) * torch.as_tensor([img.size(3) - 1, img.size(2) - 1]).float().to(
+            img.device)
 
-        self.aimpoint_classifier = torch.nn.Conv2d(c, 1, 1)
-        self.linearL = torch.nn.Linear(400*300,2)
-
-    def forward(self, x):
-        """
-           Your code here.
-           Implement a forward pass through the network, use forward for training,
-           and detect for detection
-        """
-        z = (x - self.input_mean[None, :, None, None].to(x.device)) / self.input_std[None, :, None, None].to(x.device)
-        x=z
-
-        up_activation = []
-        for i in range(self.n_conv):
-            # Add all the information required for skip connections
-            up_activation.append(z)
-            z = self._modules['conv%d' % i](z)
-
-        for i in reversed(range(self.n_conv)):
-            z = self._modules['upconv%d' % i](z)
-            # Fix the padding
-            z = z[:, :, :up_activation[i].size(2), :up_activation[i].size(3)]
-            # Add the skip connection
-            if self.use_skip:
-                z = torch.cat([z, up_activation[i]], dim=1)
-
-        
-        z = self.aimpoint_classifier(z)
-
-        #z = self.linearL(z.view(z.size(0), -1))
-        z = spatial_argmax(z[:, 0])
-
-        #print (z[0])
-        #return z
-        return z
-        #return (1 + spatial_argmax(z.squeeze(1))) * torch.as_tensor([x.size(3) - 1, x.size(2) - 1]).float().to(
-           # x.device)
 
 def save_model(model):
     from torch import save
@@ -119,7 +55,8 @@ def save_model(model):
 def load_model():
     from torch import load
     from os import path
-    print ("--------------------------------IN LOAD MODEL")
     r = Planner()
-    r.load_state_dict(load(path.join(path.dirname(path.abspath(__file__)), '/content/cs342/final/image_agent/planner.th'), map_location='cpu'))
+    r.load_state_dict(load(path.join(path.dirname(path.abspath(__file__)), 'planner.th'), map_location='cpu'))
     return r
+
+
