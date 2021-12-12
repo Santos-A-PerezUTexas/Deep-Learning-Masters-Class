63639b6378fc256ec0da6f72b0249a543a6c6dcf
Santos-A-PerezUTexas <saperez@utexas.edu>
2021-12-12 03:17:33 +0000

    Update From Colab

--- final/image_agent/planner.py
+++ final/image_agent/planner.py
@@ -12,10 +12,10 @@ def spatial_argmax(logit):
     
                         
 class Planner(torch.nn.Module):
-    def __init__(self, channels=[16, 32, 64, 128]):
+    def __init__(self, channels=[16, 32, 64, 32]):
         super().__init__()
 
-        conv_block = lambda c, h: [torch.nn.BatchNorm2d(h), torch.nn.Conv2d(h, c, 7, 2, 3), torch.nn.ReLU(True)]
+        conv_block = lambda c, h: [torch.nn.BatchNorm2d(h), torch.nn.Conv2d(h, c, 5, 2, 2), torch.nn.ReLU(True)]
         upconv_block = lambda c, h: [torch.nn.BatchNorm2d(h), torch.nn.ConvTranspose2d(h, c, 4, 2, 1),
                                      torch.nn.ReLU(True)]
 
@@ -40,8 +40,14 @@ class Planner(torch.nn.Module):
         img = (img - self._mean[None, :, None, None].to(img.device)) / self._std[None, :, None, None].to(img.device)
         h = self._conv(img)
         x = self._upconv(h)
-        return (1 + spatial_argmax(x.squeeze(1))) * torch.as_tensor([img.size(3) - 1, img.size(2) - 1]).float().to(
+
+        output = (1 + spatial_argmax(x.squeeze(1))) 
+        width = img.size(3)
+        height = img.size(2)
+        output = output * torch.as_tensor([width - 1,    height - 1]).float().to(
             img.device)
+
+        return  output #300/400 range
 
         #return(x)
 
@@ -60,37 +66,4 @@ def load_model():
     r.load_state_dict(load(path.join(path.dirname(path.abspath(__file__)), 'planner.th'), map_location='cpu'))
     return r
 
-"""
-class Planner(torch.nn.Module):
-    def __init__(self, channels=[16, 32]):  #[16, 32, 64, 128]
-        super().__init__()
 
-        conv_block = lambda c, h: [torch.nn.BatchNorm2d(h), torch.nn.Conv2d(h, c, 7, 2, 3), torch.nn.ReLU(True)]
-        upconv_block = lambda c, h: [torch.nn.BatchNorm2d(h), torch.nn.ConvTranspose2d(h, c, 4, 2, 1),
-                                     torch.nn.ReLU(True)]
-
-        h, _conv, _upconv = 3, [], []
-        for c in channels:
-            _conv += conv_block(c, h)
-            h = c
-
-        for c in channels[:-3:-1]:
-            _upconv += upconv_block(c, h)
-            h = c
-
-        _upconv += [torch.nn.BatchNorm2d(h), torch.nn.Conv2d(h, 1, 1, 1, 0)]
-
-        self._conv = torch.nn.Sequential(*_conv)
-        self._upconv = torch.nn.Sequential(*_upconv)   
-        self._mean = torch.FloatTensor([0.4519, 0.5590, 0.6204])
-        self._std = torch.FloatTensor([0.0012, 0.0018, 0.0020])
-
-    def forward(self, img):
-        
-        img = (img - self._mean[None, :, None, None].to(img.device)) / self._std[None, :, None, None].to(img.device)
-        h = self._conv(img)
-        x = self._upconv(h)
-        return (1 + spatial_argmax(x.squeeze(1))) * torch.as_tensor([img.size(3) - 1, img.size(2) - 1]).float().to(
-            img.device)
-
-"""
